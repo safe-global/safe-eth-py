6242f9b34e3d0579ae05804f4b5aecf8aa1d1110
Uxio Fuentefria <uxio@gnosis.pm>
2018-08-31 15:43:28 +0200

    Include abis into package

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='gnosis-py',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
