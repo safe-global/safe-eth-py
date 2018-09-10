f957594979fa3f1a6766cbba626cac8c7bf56144
Uxio Fuentefria <uxio@gnosis.pm>
2018-09-10 14:00:21 +0200

    Add script for running tests

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ps aux | grep ganache-cli | grep -v grep > /dev/null
+if [ $? -eq 1 ]; then
+    echo 'Running Ganache-Cli'
+    ganache-cli -d --defaultBalanceEther 10000 -a 10 --noVMErrorsOnRPCResponse &
+    GANACHE_PID=$!
+    sleep 3
+fi
+
+python manage.py test --settings=config.settings.test
+
+if [ ${GANACHE_PID:-0} -gt 1 ]; then
+    echo 'Killing opened Ganache-Cli'
+    kill $GANACHE_PID
+fi
