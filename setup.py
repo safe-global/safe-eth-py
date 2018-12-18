95648ec01bc827ff0c1b6b2b301f8051a3965691
Uxio Fuentefria <uxio@gnosis.pm>
2018-12-18 18:18:56 +0100

    Set version 0.7.9

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name='gnosis-py',
-    version='0.7.8',
+    version='0.7.9',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
