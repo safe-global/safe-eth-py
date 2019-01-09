bb7b108ad2cd1a81d58b30f3d49b3418e80a60ae
Uxio Fuentefria <uxio@gnosis.pm>
2019-01-09 13:21:45 +0100

    Set version 0.9.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name='gnosis-py',
-    version='0.8.1',
+    version='0.9.0',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
