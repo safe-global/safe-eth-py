6fafff5a67cfa15e06d11e21cf5a6d22ebc51ed4
Uxio Fuentefria <uxio@gnosis.pm>
2019-11-12 14:17:11 +0100

    Fix bug with signatures when using `str`

--- gnosis/safe/signatures.py
+++ gnosis/safe/signatures.py
@@ -1,14 +1,16 @@
 from typing import List, Tuple, Union
 
 from ethereum.utils import checksum_encode, ecrecover_to_pub, sha3
+from hexbytes import HexBytes
 
 
-def signature_split(signatures: bytes, pos: int = 0) -> Tuple[int, int, int]:
+def signature_split(signatures: Union[bytes, str], pos: int = 0) -> Tuple[int, int, int]:
     """
     :param signatures: signatures in form of {bytes32 r}{bytes32 s}{uint8 v}
     :param pos: position of the signature
     :return: Tuple with v, r, s
     """
+    signatures = HexBytes(signatures)
     signature_pos = 65 * pos
     v = signatures[64 + signature_pos]
     r = int.from_bytes(signatures[signature_pos:32 + signature_pos], 'big')
@@ -46,6 +48,6 @@ def get_signing_address(signed_hash: Union[bytes, str], v: int, r: int, s: int) 
     :return: checksummed ethereum address, for example `0x568c93675A8dEb121700A6FAdDdfE7DFAb66Ae4A`
     :rtype: str
     """
-    encoded_64_address = ecrecover_to_pub(signed_hash, v, r, s)
+    encoded_64_address = ecrecover_to_pub(HexBytes(signed_hash), v, r, s)
     address_bytes = sha3(encoded_64_address)[-20:]
     return checksum_encode(address_bytes)
