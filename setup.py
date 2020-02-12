c09e99ae6a82b4bb4f14c3563bac3d6f0ef8fd5c
Uxio Fuentefria <uxio@gnosis.pm>
2020-02-12 13:43:14 +0100

    Set version 2.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name='gnosis-py',
-    version='2.0.10',
+    version='2.1.0',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
