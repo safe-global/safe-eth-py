6bf3436089f06e9d55b2f824510b562ed20f5c85
Uxio Fuentefria <uxio@gnosis.pm>
2020-03-30 14:57:58 +0200

    Refactor SafeSignature classes

--- gnosis/safe/safe_signature.py
+++ gnosis/safe/safe_signature.py
@@ -1,14 +1,17 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 from logging import getLogger
-from typing import Iterable, Union
+from typing import List, Union
 
+from eth_abi import encode_single
 from eth_account.messages import defunct_hash_message
 from ethereum.utils import checksum_encode
 from hexbytes import HexBytes
 
 from gnosis.eth import EthereumClient
 from gnosis.eth.contracts import get_safe_contract
-from gnosis.safe.signatures import get_signing_address, signature_split
+from gnosis.safe.signatures import (get_signing_address, signature_split,
+                                    signature_to_bytes)
 
 logger = getLogger(__name__)
 
@@ -19,7 +22,7 @@ EthereumBytes = Union[bytes, str]
 class SafeSignatureType(Enum):
     CONTRACT_SIGNATURE = 0
     APPROVED_HASH = 1
-    EOA_SIGNATURE = 2
+    EOA = 2
     ETH_SIGN = 3
 
     @staticmethod
@@ -31,76 +34,145 @@ class SafeSignatureType(Enum):
         elif v > 30:
             return SafeSignatureType.ETH_SIGN
         else:
-            return SafeSignatureType.EOA_SIGNATURE
+            return SafeSignatureType.EOA
 
 
-# TODO Create method to generate signature based on multiple `SafeSignature`. This is very important to
-# prepare the payloads for CONTRACT_SIGNATUREs. Also, signatures must be sorted by owner
-class SafeSignature:
-    EIP1271_MAGIC_VALUE = HexBytes(0x20c13b0b)
-
-    def __init__(self, signature: EthereumBytes, safe_tx_hash: EthereumBytes, contract_signature: EthereumBytes = b''):
+class SafeSignature(ABC):
+    def __init__(self, signature: EthereumBytes, safe_tx_hash: EthereumBytes):
         self.signature = HexBytes(signature)
         self.safe_tx_hash = safe_tx_hash
-        self.contract_signature = contract_signature
-
         self.v, self.r, self.s = signature_split(self.signature)
-        self.signature_type = SafeSignatureType.from_v(self.v)
-
-    @property
-    def owner(self):
-        """
-        :return: Decode owner from signature, without any further validation (signature can be not valid)
-        """
-        v, r, s, safe_tx_hash = self.v, self.r, self.s, self.safe_tx_hash
-        if v == 0:  # Contract signature
-            # We don't need further checks to get the owner
-            contract_address = checksum_encode(r)
-            return contract_address
-        elif v == 1:  # Approved hash
-            return checksum_encode(r)
-        elif v > 30:  # Eth_sign, v = v + 4 to be different from EOA signature
-            # defunct_hash_message prepends `\x19Ethereum Signed Message:\n32`
-            message_hash = defunct_hash_message(primitive=safe_tx_hash)
-            return get_signing_address(message_hash, v - 4, r, s)
-        else:  # EOA signature
-            return get_signing_address(safe_tx_hash, v, r, s)
-
-    def is_valid(self, ethereum_client: EthereumClient, safe_address: str) -> bool:
-        """
-        :param ethereum_client: Required for Contract Signature and Approved Hash check
-        :param safe_address: Required for Approved Hash check
-        :return: `True` if signature is valid, `False` otherwise
-        """
-        if self.signature_type == SafeSignatureType.CONTRACT_SIGNATURE:
-            safe_contract = get_safe_contract(ethereum_client.w3, self.owner)
-            return safe_contract.functions.isValidSignature(self.safe_tx_hash,
-                                                            self.contract_signature
-                                                            ).call() == self.EIP1271_MAGIC_VALUE
-        elif self.signature_type == SafeSignatureType.APPROVED_HASH:
-            safe_contract = get_safe_contract(ethereum_client.w3, safe_address)
-            return safe_contract.functions.approvedHashes(self.owner,
-                                                          self.safe_tx_hash).call() == 1
-        else:  # EOA and Eth sign must be validated
-            return True
 
     @classmethod
-    def parse_signatures(cls, signatures: EthereumBytes, safe_tx_hash: EthereumBytes) -> Iterable['SafeSignature']:
+    def parse_signatures(cls, signatures: EthereumBytes, safe_tx_hash: EthereumBytes) -> List['SafeSignature']:
+        """
+        :param signatures: One or more signatures appended. EIP1271 data at the end is supported.
+        :param safe_tx_hash:
+        :return: List of SafeSignatures decoded
+        """
         signature_size = 65  # For contract signatures there'll be some data at the end
         data_position = len(signatures)  # For contract signatures, to stop parsing at data position
 
+        safe_signatures = []
         for i in range(0, len(signatures), signature_size):
             if i >= data_position:  # If contract signature data position is reached, stop
                 break
 
             signature = signatures[i: i + signature_size]
             v, r, s = signature_split(signature)
-            if v == 0:  # Contract signature
+            signature_type = SafeSignatureType.from_v(v)
+            if signature_type == SafeSignatureType.CONTRACT_SIGNATURE:
                 if s < data_position:
                     data_position = s
                 contract_signature_len = int.from_bytes(signatures[s:s + 32], 'big')  # Len size is 32 bytes
-                contract_signature = signatures[s + 32:s + 32 + contract_signature_len]  # Skip array size (32 bytes)
-            else:
-                contract_signature = None
+                contract_signature = HexBytes(signatures[s + 32:
+                                                         s + 32 + contract_signature_len])  # Skip array size (32 bytes)
+                safe_signature = SafeSignatureContract(signature, safe_tx_hash, contract_signature)
+            elif signature_type == SafeSignatureType.APPROVED_HASH:
+                safe_signature = SafeSignatureApprovedHash(signature, safe_tx_hash)
+            elif signature_type == SafeSignatureType.EOA:
+                safe_signature = SafeSignatureEOA(signature, safe_tx_hash)
+            elif signature_type == SafeSignatureType.ETH_SIGN:
+                safe_signature = SafeSignatureEthSign(signature, safe_tx_hash)
 
-            yield cls(signature, safe_tx_hash, contract_signature=contract_signature)
+            safe_signatures.append(safe_signature)
+        return safe_signatures
+
+    def export_signature(self) -> HexBytes:
+        """
+        Exports signature in a format that's valid individually. That's important for contract signatures, as it
+        will fix the offset
+        :return:
+        """
+        return self.signature
+
+    @property
+    @abstractmethod
+    def owner(self):
+        """
+        :return: Decode owner from signature, without any further validation (signature can be not valid)
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def is_valid(self, ethereum_client: EthereumClient, safe_address: str) -> bool:
+        """
+        :param ethereum_client: Required for Contract Signature and Approved Hash check
+        :param safe_address: Required for Approved Hash check
+        :return: `True` if signature is valid, `False` otherwise
+        """
+        raise NotImplemented
+
+
+class SafeSignatureContract(SafeSignature):
+    EIP1271_MAGIC_VALUE = HexBytes(0x20c13b0b)
+
+    def __init__(self, signature: EthereumBytes, safe_tx_hash: EthereumBytes, contract_signature: EthereumBytes):
+        super().__init__(signature, safe_tx_hash)
+        self.contract_signature = contract_signature
+
+    @property
+    def owner(self):
+        # We don't need further checks to get the owner
+        contract_address = checksum_encode(self.r)
+        return contract_address
+
+    @property
+    def signature_type(self):
+        return SafeSignatureType.CONTRACT_SIGNATURE
+
+    def export_signature(self) -> HexBytes:
+        """
+        Fix offset (s) and append `contract_signature` at the end of the signature
+        :return:
+        """
+        return HexBytes(signature_to_bytes((self.v, self.r, 65)) + encode_single('bytes', self.contract_signature))
+
+    def is_valid(self, ethereum_client: EthereumClient, *args) -> bool:
+        safe_contract = get_safe_contract(ethereum_client.w3, self.owner)
+        return safe_contract.functions.isValidSignature(self.safe_tx_hash,
+                                                        self.contract_signature
+                                                        ).call() == self.EIP1271_MAGIC_VALUE
+
+
+class SafeSignatureApprovedHash(SafeSignature):
+    @property
+    def owner(self):
+        return checksum_encode(self.r)
+
+    @property
+    def signature_type(self):
+        return SafeSignatureType.APPROVED_HASH
+
+    def is_valid(self, ethereum_client: EthereumClient, safe_address: str) -> bool:
+        safe_contract = get_safe_contract(ethereum_client.w3, safe_address)
+        return safe_contract.functions.approvedHashes(self.owner,
+                                                      self.safe_tx_hash).call() == 1
+
+
+class SafeSignatureEthSign(SafeSignature):
+    @property
+    def owner(self):
+        # defunct_hash_message prepends `\x19Ethereum Signed Message:\n32`
+        message_hash = defunct_hash_message(primitive=self.safe_tx_hash)
+        return get_signing_address(message_hash, self.v - 4, self.r, self.s)
+
+    @property
+    def signature_type(self):
+        return SafeSignatureType.ETH_SIGN
+
+    def is_valid(self, *args) -> bool:
+        return True
+
+
+class SafeSignatureEOA(SafeSignature):
+    @property
+    def owner(self):
+        return get_signing_address(self.safe_tx_hash, self.v, self.r, self.s)
+
+    @property
+    def signature_type(self):
+        return SafeSignatureType.EOA
+
+    def is_valid(self, *args) -> bool:
+        return True
--- gnosis/safe/tests/test_safe_signature.py
+++ gnosis/safe/tests/test_safe_signature.py
@@ -10,28 +10,32 @@ from hexbytes import HexBytes
 from web3 import Web3
 
 from .. import Safe
-from ..safe_signature import SafeSignature, SafeSignatureType
+from ..safe_signature import (SafeSignature, SafeSignatureApprovedHash,
+                              SafeSignatureContract, SafeSignatureEOA,
+                              SafeSignatureEthSign, SafeSignatureType)
 from .safe_test_case import SafeTestCaseMixin
 
 logger = logging.getLogger(__name__)
 
 
-class TestSignature(TestCase):
+class TestSafeSignature(TestCase):
     def test_contract_signature(self):
         owner = '0x05c85Ab5B09Eb8A55020d72daf6091E04e264af9'
         safe_tx_hash = HexBytes('0x4c9577d1b1b8dec52329a983ae26238b65f74b7dd9fb28d74ad9548e92aaf196')
         signature = HexBytes('0x00000000000000000000000005c85ab5b09eb8a55020d72daf6091e04e264af900000000000000000000000'
                              '0000000000000000000000000000000000000000000')
-        safe_signature = SafeSignature(signature, safe_tx_hash)
+        safe_signature = SafeSignatureContract(signature, safe_tx_hash, b'')
         self.assertEqual(safe_signature.owner, owner)
+        self.assertEqual(safe_signature.signature_type, SafeSignatureType.CONTRACT_SIGNATURE)
 
     def test_approved_hash_signature(self):
         owner = '0x05c85Ab5B09Eb8A55020d72daf6091E04e264af9'
         safe_tx_hash = HexBytes('0x4c9577d1b1b8dec52329a983ae26238b65f74b7dd9fb28d74ad9548e92aaf196')
         signature = HexBytes('0x00000000000000000000000005c85ab5b09eb8a55020d72daf6091e04e264af900000000000000000000000'
                              '0000000000000000000000000000000000000000001')
-        safe_signature = SafeSignature(signature, safe_tx_hash)
+        safe_signature = SafeSignatureApprovedHash(signature, safe_tx_hash)
         self.assertEqual(safe_signature.owner, owner)
+        self.assertEqual(safe_signature.signature_type, SafeSignatureType.APPROVED_HASH)
 
     def test_eth_sign_signature(self):
         account = Account.create()
@@ -40,23 +44,29 @@ class TestSignature(TestCase):
         message = defunct_hash_message(primitive=safe_tx_hash)
         signature = account.signHash(message)['signature']
         signature = signature[:64] + HexBytes(signature[64] + 4)  # Add 4 to v
-        safe_signature = SafeSignature(signature, safe_tx_hash)
+        safe_signature = SafeSignatureEthSign(signature, safe_tx_hash)
         self.assertEqual(safe_signature.owner, owner)
+        self.assertEqual(safe_signature.signature_type, SafeSignatureType.ETH_SIGN)
+        self.assertTrue(safe_signature.is_valid())
 
     def test_eoa_signature(self):
         owner = '0xADb7CB706e9A1bd9F96a397da340bF34a9984E1E'
         safe_tx_hash = HexBytes('0x4c9577d1b1b8dec52329a983ae26238b65f74b7dd9fb28d74ad9548e92aaf196')
         signature = HexBytes('0x5dccf9f1375cee56331a67867a0b05a828894c2b76dee84546ec663997d7548257cb2d606087ee7590b966e'
                              '958d97a65528ae941a0f7e5050949f618629509c81b')
-        safe_signature = SafeSignature(signature, safe_tx_hash)
+        safe_signature = SafeSignatureEOA(signature, safe_tx_hash)
         self.assertEqual(safe_signature.owner, owner)
+        self.assertEqual(safe_signature.signature_type, SafeSignatureType.EOA)
+        self.assertTrue(safe_signature.is_valid())
 
         account = Account.create()
         owner = account.address
         safe_tx_hash = HexBytes('0x123477d1b1b8dec52329a983ae26238b65f74b7dd9fb28d74ad9548e92aaf195')
         signature = account.signHash(safe_tx_hash)['signature']
-        safe_signature = SafeSignature(signature, safe_tx_hash)
+        safe_signature = SafeSignatureEOA(signature, safe_tx_hash)
         self.assertEqual(safe_signature.owner, owner)
+        self.assertEqual(safe_signature.signature_type, SafeSignatureType.EOA)
+        self.assertTrue(safe_signature.is_valid())
 
     def test_defunct_hash_message(self):
         safe_tx_hash = HexBytes('0x4c9577d1b1b8dec52329a983ae26238b65f74b7dd9fb28d74ad9548e92aaf196')
@@ -76,10 +86,15 @@ class TestSignature(TestCase):
 
         s1, s2 = SafeSignature.parse_signatures(signatures, safe_tx_hash)
         self.assertEqual(s1.owner, owner_1)
+        self.assertEqual(s1.signature_type, SafeSignatureType.APPROVED_HASH)
+        self.assertIsInstance(s1, SafeSignatureApprovedHash)
+        self.assertEqual(s2.signature_type, SafeSignatureType.EOA)
         self.assertEqual(s2.owner, owner_2)
+        self.assertTrue(s2.is_valid())
+        self.assertIsInstance(s2, SafeSignatureEOA)
 
 
-class TestContractSignature(SafeTestCaseMixin, TestCase):
+class TestSafeContractSignature(SafeTestCaseMixin, TestCase):
     def test_contract_signature(self):
         owner_1 = self.ethereum_test_account
         deployed_safe = self.deploy_test_safe(owners=[owner_1.address],
@@ -93,7 +108,7 @@ class TestContractSignature(SafeTestCaseMixin, TestCase):
         contract_signature = encode_single('bytes', b'')
         signature = signature_r + signature_s + signature_v + contract_signature
 
-        safe_signature = next(SafeSignature.parse_signatures(signature, safe_tx_hash))
+        safe_signature = SafeSignature.parse_signatures(signature, safe_tx_hash)[0]
         self.assertFalse(safe_signature.is_valid(self.ethereum_client, None))
 
         # Check with previously signedMessage
@@ -104,12 +119,13 @@ class TestContractSignature(SafeTestCaseMixin, TestCase):
         safe_tx.sign(owner_1.key)
         safe_tx.execute(owner_1.key)
 
-        safe_signature = next(SafeSignature.parse_signatures(signature, safe_tx_hash))
+        safe_signature = SafeSignature.parse_signatures(signature, safe_tx_hash)[0]
         self.assertTrue(safe_signature.is_valid(self.ethereum_client, None))
+        self.assertIsInstance(safe_signature, SafeSignatureContract)
 
         # Check with crafted signature
         safe_tx_hash_2 = Web3.keccak(text='test2')
-        safe_signature = next(SafeSignature.parse_signatures(signature, safe_tx_hash_2))
+        safe_signature = SafeSignature.parse_signatures(signature, safe_tx_hash_2)[0]
         self.assertFalse(safe_signature.is_valid(self.ethereum_client, None))
 
         safe_tx_hash_2_message_hash = safe_contract.functions.getMessageHash(safe_tx_hash_2).call()
@@ -119,7 +135,7 @@ class TestContractSignature(SafeTestCaseMixin, TestCase):
         # 96 - 65 = `31`
         self.assertEqual(len(encoded_contract_signature), len(contract_signature) + 32 + 31)
         crafted_signature = signature_r + signature_s + signature_v + encoded_contract_signature
-        safe_signature = next(SafeSignature.parse_signatures(crafted_signature, safe_tx_hash_2))
+        safe_signature = SafeSignature.parse_signatures(crafted_signature, safe_tx_hash_2)[0]
         self.assertEqual(contract_signature, safe_signature.contract_signature)
         self.assertTrue(safe_signature.is_valid(self.ethereum_client, None))
 
@@ -166,5 +182,9 @@ class TestContractSignature(SafeTestCaseMixin, TestCase):
             self.assertEqual(safe_signature.contract_signature, contract_signature)
             self.assertTrue(safe_signature.is_valid(self.ethereum_client, None))
             self.assertEqual(safe_signature.signature_type, SafeSignatureType.CONTRACT_SIGNATURE)
+            # Test exported signature
+            exported_signature = SafeSignature.parse_signatures(safe_signature.export_signature(), safe_tx_hash)[0]
+            self.assertEqual(exported_signature.contract_signature, safe_signature.contract_signature)
+            self.assertTrue(exported_signature.is_valid(self.ethereum_client, None))
             count += 1
         self.assertEqual(count, 2)
