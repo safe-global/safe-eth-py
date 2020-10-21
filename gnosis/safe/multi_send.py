5409ec434f924278cc54bbe2508f057c04410ed6
Uxio Fuentefria <uxio@gnosis.pm>
2020-10-21 17:51:01 +0200

    Add support for old MultiSend encoded parameters

--- gnosis/safe/multi_send.py
+++ gnosis/safe/multi_send.py
@@ -20,11 +20,23 @@ class MultiSendOperation(Enum):
 
 
 class MultiSendTx:
-    def __init__(self, operation: MultiSendOperation, to: str, value: int, data: EthereumData):
+    """
+    Wrapper for a single MultiSendTx
+    """
+    def __init__(self, operation: MultiSendOperation, to: str, value: int, data: EthereumData,
+                 old_encoding: bool = False):
+        """
+        :param operation: MultisendOperation, CALL or DELEGATE_CALL
+        :param to: Address
+        :param value: Value in Wei
+        :param data: data as hex string or bytes
+        :param old_encoding: True if using old multisend ABI Encoded data, False otherwise
+        """
         self.operation = operation
         self.to = to
         self.value = value
         self.data = HexBytes(data) if data else b''
+        self.old_encoding = old_encoding
 
     def __eq__(self, other):
         if not isinstance(other, MultiSendTx):
@@ -39,10 +51,10 @@ class MultiSendTx:
         """
         return 21 + 32 * 2 + self.data_length
 
-    def __str__(self):
+    def __repr__(self):
         data = self.data[:4].hex() + ('...' if len(self.data) > 4 else '')
-        return f'MultisendTx operation={self.operation.name} to={self.to} value={self.value}' \
-               f' data={data}'
+        return f'MultisendTx operation={self.operation.name} to={self.to} value={self.value} ' \
+               f'data={data}'
 
     @property
     def data_length(self) -> int:
@@ -59,29 +71,74 @@ class MultiSendTx:
     @classmethod
     def from_bytes(cls, encoded_multisend_tx: Union[str, bytes]) -> 'MultiSendTx':
         """
-        Decodes one multisend transaction. If there's more data after `data` it's ignored. Structure:
-        operation   -> MultiSendOperation 1 byte
-        to          -> ethereum address 20 bytes
-        value       -> tx value 32 bytes
-        data_length -> 32 bytes
-        data        -> `data_length` bytes
+        Decoded one MultiSend transaction. ABI must be used to get the `transactions` parameter and use that data
+        for this function
+        :param encoded_multisend_tx:
+        :return:
+        """
+        encoded_multisend_tx = HexBytes(encoded_multisend_tx)
+        try:
+            return cls._decode_multisend_data(encoded_multisend_tx)
+        except ValueError:
+            # Try using the old decoding method
+            return cls._decode_multisend_old_transaction(encoded_multisend_tx)
+
+    @classmethod
+    def _decode_multisend_data(cls, encoded_multisend_tx: Union[str, bytes]):
+        """
+        Decodes one Multisend transaction. If there's more data after `data` it's ignored. Fallbacks to the old
+        multisend structure if this structure cannot be decoded.
+        https://etherscan.io/address/0x8D29bE29923b68abfDD21e541b9374737B49cdAD#code
+        Structure:
+            - operation   -> MultiSendOperation 1 byte
+            - to          -> ethereum address 20 bytes
+            - value       -> tx value 32 bytes
+            - data_length -> 32 bytes
+            - data        -> `data_length` bytes
         :param encoded_multisend_tx: 1 multisend transaction encoded
         :return: Tx as a MultisendTx
         """
         encoded_multisend_tx = HexBytes(encoded_multisend_tx)
-
         operation = MultiSendOperation(encoded_multisend_tx[0])
         to = Web3.toChecksumAddress(encoded_multisend_tx[1:1 + 20])
         value = int.from_bytes(encoded_multisend_tx[21:21 + 32], byteorder='big')
         data_length = int.from_bytes(encoded_multisend_tx[21 + 32: 21 + 32 * 2], byteorder='big')
         data = encoded_multisend_tx[21 + 32 * 2: 21 + 32 * 2 + data_length]
-        if data_length != len(data):
-            raise ValueError('Data length is different from len(data)')
+        len_data = len(data)
+        if len_data != data_length:
+            raise ValueError(f'Data length {data_length} is different from len(data) {len_data}')
+        return cls(operation, to, value, data, old_encoding=False)
 
-        return cls(operation, to, value, data)
+    @classmethod
+    def _decode_multisend_old_transaction(cls, encoded_multisend_tx: Union[str, bytes]) -> 'MultiSendTx':
+        """
+        Decodes one old multisend transaction. If there's more data after `data` it's ignored. The difference with
+        the new MultiSend is that every value but `data` is padded to 32 bytes, wasting a lot of bytes.
+        https://etherscan.io/address/0xE74d6AF1670FB6560dd61EE29eB57C7Bc027Ce4E#code
+        Structure:
+            - operation   -> MultiSendOperation 32 byte
+            - to          -> ethereum address 32 bytes
+            - value       -> tx value 32 bytes
+            - data_length -> 32 bytes
+            - data        -> `data_length` bytes
+        :param encoded_multisend_tx: 1 multisend transaction encoded
+        :return: Tx as a MultisendTx
+        """
+        encoded_multisend_tx = HexBytes(encoded_multisend_tx)
+        operation = MultiSendOperation(int.from_bytes(encoded_multisend_tx[:32], byteorder='big'))
+        to = Web3.toChecksumAddress(encoded_multisend_tx[32:64][-20:])
+        value = int.from_bytes(encoded_multisend_tx[64:96], byteorder='big')
+        data_length = int.from_bytes(encoded_multisend_tx[128:160], byteorder='big')
+        data = encoded_multisend_tx[160:160 + data_length]
+        len_data = len(data)
+        if len_data != data_length:
+            raise ValueError(f'Data length {data_length} is different from len(data) {len_data}')
+        return cls(operation, to, value, data, old_encoding=True)
 
 
 class MultiSend:
+    dummy_w3 = Web3()
+
     def __init__(self, address: str, ethereum_client: EthereumClient):
         assert Web3.isChecksumAddress(address), \
             '%s proxy factory address not valid' % address
@@ -105,7 +162,11 @@ class MultiSend:
         multisend_tx_size = len(multisend_tx)
 
         assert multisend_tx_size > 0, 'Multisend tx cannot be empty'  # This should never happen, just in case
-        remaining_data = encoded_multisend_txs[multisend_tx_size:]
+        if multisend_tx.old_encoding:
+            next_data_position = ((multisend_tx.data_length + 0x1f) // 0x20 * 0x20) + 0xa0
+        else:
+            next_data_position = multisend_tx_size
+        remaining_data = encoded_multisend_txs[next_data_position:]
 
         return [multisend_tx] + cls.from_bytes(remaining_data)
 
@@ -116,7 +177,7 @@ class MultiSend:
         :return:
         """
         try:
-            _, data = get_multi_send_contract(Web3()).decode_function_input(multisend_data)
+            _, data = get_multi_send_contract(cls.dummy_w3).decode_function_input(multisend_data)
             return cls.from_bytes(data['transactions'])
         except ValueError:
             return []
