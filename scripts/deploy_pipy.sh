d914a620186c44c1de959134c2f286c4b01c5f56
Uxio Fuentefria <uxio@gnosis.pm>
2021-02-02 14:16:53 +0100

    Configure github actions
    - Remove travis

--- scripts/deploy_pipy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# fail if any commands fails
-set -e
-
-python -m pip install --upgrade setuptools wheel
-python -m pip install --upgrade twine
-
-python setup.py sdist bdist_wheel
-twine upload -u "${1}" -p "${2}" dist/*
