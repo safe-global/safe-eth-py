7244e4c73c84ef7c254bdac25c62c7badf4fdcc6
Uxio Fuentefria <uxio@gnosis.pm>
2021-02-10 16:31:28 +0100

    Set version 2.7.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name='gnosis-py',
-    version='2.7.7',
+    version='2.7.8',
     packages=find_packages(),
     package_data={'gnosis': ['py.typed']},
     install_requires=requirements,
