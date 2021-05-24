c7e8870854765907bdd5b2c04c13fd4d97092a6a
Uxio Fuentefria <uxio@gnosis.pm>
2021-05-24 12:47:20 +0200

    Fix mainnet node testing issues

--- gnosis/eth/tests/utils.py
+++ gnosis/eth/tests/utils.py
@@ -13,8 +13,12 @@ def just_test_if_mainnet_node() -> str:
     mainnet_node_url = os.environ.get('ETHEREUM_MAINNET_NODE')
     if not mainnet_node_url:
         pytest.skip("Mainnet node not defined, cannot test oracles", allow_module_level=True)
-    elif requests.get(mainnet_node_url, timeout=5).status_code == 404:
-        pytest.skip("Cannot connect to mainnet node", allow_module_level=True)
+    else:
+        try:
+            if not requests.get(mainnet_node_url, timeout=5).ok:
+                pytest.skip("Cannot connect to mainnet node", allow_module_level=True)
+        except IOError:
+            pytest.skip("Problem connecting to the mainnet node", allow_module_level=True)
     return mainnet_node_url
 
 
