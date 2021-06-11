bd176f7a9daabccd6f5995b34127f67797ba3960
Uxio Fuentefria <uxio@gnosis.pm>
2021-06-11 11:06:29 +0200

    Set version 3.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
 
 setup(
     name='gnosis-py',
-    version='3.1.0',
+    version='3.1.1',
     packages=find_packages(),
     package_data={'gnosis': ['py.typed']},
     install_requires=requirements,
