637ce5c92f208f0e1d4f5e0687c10f378cfdff56
Uxio Fuentefria <uxio@gnosis.pm>
2021-08-23 02:30:45 +0200

    Set version 3.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
 
 setup(
     name='gnosis-py',
-    version='3.1.16',
+    version='3.2.0',
     packages=find_packages(),
     package_data={'gnosis': ['py.typed']},
     install_requires=requirements,
