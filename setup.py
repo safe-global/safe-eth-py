b0bbbcbd487360fba8efccbf8ae9b1bdc144b1f9
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-08-24 13:07:45 +0200

    Bump version to 3.2.1 (#59)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
 
 setup(
     name='gnosis-py',
-    version='3.2.0',
+    version='3.2.1',
     packages=find_packages(),
     package_data={'gnosis': ['py.typed']},
     install_requires=requirements,
