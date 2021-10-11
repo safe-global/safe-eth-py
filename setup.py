8a9eb29d3544a5e192df8f1cd283dd01944dd0a8
Mikhail <mmvsha73@gmail.com>
2021-10-11 17:13:09 +0200

    set version number to 3.5.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ extras_require = {
 
 setup(
     name='gnosis-py',
-    version='3.4.4',
+    version='3.5.0',
     packages=find_packages(),
     package_data={'gnosis': ['py.typed']},
     install_requires=requirements,
