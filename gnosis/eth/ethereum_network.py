abeab4d230018866a6b9a0aa44a418c04bf5c907
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-08-16 22:17:29 +0800

    feat: add Arbitrum Nova as ARBITRUM_NOVA

--- gnosis/eth/ethereum_network.py
+++ gnosis/eth/ethereum_network.py
@@ -159,7 +159,7 @@ class EthereumNetwork(Enum):
     FSN = 32659
     NRG = 39797
     ARBITRUM = 42161
-    ARB_NOVA = 42170
+    ARBITRUM_NOVA = 42170
     CELO = 42220
     ATH_ATHEREUM = 43110
     AVALANCHE = 43114
