a3b13bc78fc0244e9b2217c42325a7da70c3561a
the-amazing-ultraton <106994129+the-amazing-ultraton@users.noreply.github.com>
2022-08-23 15:32:15 -0300

    feat: register moonbeam, moonriver, and moonbase etherscan urls

--- gnosis/eth/clients/etherscan_client.py
+++ gnosis/eth/clients/etherscan_client.py
@@ -33,6 +33,9 @@ class EtherscanClient:
         EthereumNetwork.OPTIMISTIC: "https://optimistic.etherscan.io",
         EthereumNetwork.ARBITRUM: "https://arbiscan.io",
         EthereumNetwork.AVALANCHE: "https://snowtrace.io",
+        EthereumNetwork.MOON_MOONBEAM: "https://moonscan.io",
+        EthereumNetwork.MOON_MOONRIVER: "https://moonriver.moonscan.io",
+        EthereumNetwork.MOON_MOONBASE: "https://moonbase.moonscan.io",
     }
 
     NETWORK_WITH_API_URL = {
@@ -46,6 +49,9 @@ class EtherscanClient:
         EthereumNetwork.OPTIMISTIC: "https://api-optimistic.etherscan.io",
         EthereumNetwork.ARBITRUM: "https://api.arbiscan.io",
         EthereumNetwork.AVALANCHE: "https://api.snowtrace.io",
+        EthereumNetwork.MOON_MOONBEAM: "https://api-moonbeam.moonscan.io",
+        EthereumNetwork.MOON_MOONRIVER: "https://api-moonriver.moonscan.io",
+        EthereumNetwork.MOON_MOONBASE: "https://api-moonbase.moonscan.io",
     }
     HTTP_HEADERS = {
         "User-Agent": "curl/7.77.0",
