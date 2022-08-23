7c8cfd519e490b77703b24724ad623a6e5fb9537
the-amazing-ultraton <106994129+the-amazing-ultraton@users.noreply.github.com>
2022-08-23 15:33:52 -0300

    feat: register velas etherscan urls

--- gnosis/eth/clients/etherscan_client.py
+++ gnosis/eth/clients/etherscan_client.py
@@ -33,6 +33,7 @@ class EtherscanClient:
         EthereumNetwork.OPTIMISTIC: "https://optimistic.etherscan.io",
         EthereumNetwork.ARBITRUM: "https://arbiscan.io",
         EthereumNetwork.AVALANCHE: "https://snowtrace.io",
+        EthereumNetwork.VELAS_MAINNET: "https://evmexplorer.velas.com",
     }
 
     NETWORK_WITH_API_URL = {
@@ -46,6 +47,7 @@ class EtherscanClient:
         EthereumNetwork.OPTIMISTIC: "https://api-optimistic.etherscan.io",
         EthereumNetwork.ARBITRUM: "https://api.arbiscan.io",
         EthereumNetwork.AVALANCHE: "https://api.snowtrace.io",
+        EthereumNetwork.VELAS_MAINNET: "https://evmexplorer.velas.com/api",
     }
     HTTP_HEADERS = {
         "User-Agent": "curl/7.77.0",
