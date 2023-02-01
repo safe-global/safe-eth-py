5e83afe6529c1ce7df153d70770a1873d1bf31da
moisses89 <moisses89.fn@gmail.com>
2023-02-01 14:12:56 +0100

    Update chains from ethereum chain list

--- scripts/chainlist/chains.py
+++ scripts/chainlist/chains.py
@@ -1,6 +1,8 @@
 import json
+import re
 import shutil
 from glob import glob
+from operator import itemgetter
 
 from git import Repo
 
@@ -17,6 +19,16 @@ def clean_resources() -> None:
         pass
 
 
+def convert_name(name: str) -> str:
+    # Change every symbol that is not a word or digit for underscore
+    name_converted = re.sub(r"[^\w\d]+", r"_", name.upper().replace(")", ""))
+    # Add underscore at the beggining if start by digit
+    if name_converted[0].isdigit():
+        name_converted = "_" + name_converted
+
+    return name_converted
+
+
 def process_chains() -> None:
     """
     Reads all JSON files in the REPO_DIR directory and processes the data
@@ -26,15 +38,23 @@ def process_chains() -> None:
     clean_resources()
     result_file = open(RESULT_FILE_PATH, "w")
     Repo.clone_from(GIT_URL, REPO_DIR)
-
+    chains = []
     for f_name in glob(REPO_DIR + "/_data/chains/*.json"):
         f = open(f_name)
         data = json.load(f)
+        chain = {
+            "name": convert_name(data["name"]),
+            "chainId": data["chainId"],
+        }
+        chains.append(chain)
 
+    # sort the list by chainId
+    chains = sorted(chains, key=itemgetter("chainId"))
+    for chain in chains:
         result_file.write(
             "{} = {}\n".format(
-                data["name"].upper().replace("-", "_").replace(" ", "_"),
-                data["chainId"],
+                chain["name"].upper(),
+                chain["chainId"],
             )
         )
 
