7fdd89ccef0b40599c6bc19d49e2594abde66d52
Uxio Fuentefria <Uxio0@users.noreply.github.com>
2023-04-17 13:55:29 +0200

    Remove velas testnet
    - It's not present on https://github.com/ethereum-lists/chains

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -276,10 +276,6 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 27572492, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 27572642, "1.3.0"),
     ],
-    EthereumNetwork.VELAS_EVM_TESTNET: [
-        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 27572492, "1.3.0+L2"),
-        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 27572642, "1.3.0"),
-    ],
     EthereumNetwork.WEMIX3_0_MAINNET: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 12651754, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 12651757, "1.3.0"),
@@ -488,9 +484,6 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 22172504),  # v1.3.0
     ],
     EthereumNetwork.VELAS_EVM_MAINNET: [
-        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 27571962),  # v1.3.0
-    ],
-    EthereumNetwork.VELAS_EVM_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 27571962),  # v1.3.0
     ],
     EthereumNetwork.WEMIX3_0_MAINNET: [
