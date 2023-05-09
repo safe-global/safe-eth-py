f83d65026b973b0b7100366647f5aea53072567a
moisses89 <7888669+moisses89@users.noreply.github.com>
2023-05-09 12:33:08 +0200

    Add safe singleton addresses mainnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -9,6 +9,7 @@ from gnosis.eth import EthereumNetwork
 
 MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.MAINNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 14981217, "1.3.0+L2"),
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 12504423, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 12504268, "1.3.0"),
         ("0x6851D6fDFAfD08c0295C392436245E5bc78B0185", 10329734, "1.2.0"),
@@ -321,7 +322,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.MAINNET: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 12504126),  # v1.3.0
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            14981216,
+        ),  # v1.3.0 safe singleton factory
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            12504126,
+        ),  # v1.3.0 default singleton factory
         ("0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B", 9084508),  # v1.1.1
         ("0x50e55Af101C777bA7A1d560a774A82eF002ced9F", 8915731),  # v1.1.0
         ("0x12302fE9c02ff50939BaAaaf415fc226C078613C", 7450116),  # v1.0.0
