1855b6a73a18f31306f0c935b4c26997c06e4e1f
moisses89 <7888669+moisses89@users.noreply.github.com>
2023-05-09 13:03:51 +0200

    Fix safeProxyFactory address on base goerli

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -500,7 +500,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 42293264),  # v1.3.0
     ],
     EthereumNetwork.BASE_GOERLI_TESTNET: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 938848),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 938696),  # v1.3.0
     ],
     EthereumNetwork.KAVA_EVM: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 2116356),  # v1.3.0
