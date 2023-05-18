d74c6691daad05478218030933538652628a26b1
bulbazavr1024 <pavel.ageykin@yandex.ru>
2023-05-18 20:21:32 +0300

    add: tenet testnet chain && contracts

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -423,6 +423,9 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 676474, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 676478, "1.3.0"),
     ],
+    EthereumNetwork.TENET_TESTNET: [
+        ("0x6dF18E1d9aEac1ccf30fd1B222926D1CC64799fA", 862920, "1.3.0+L2")
+    ],
     EthereumNetwork.MAP_MAINNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 5190553, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 5190556, "1.3.0"),
@@ -661,6 +664,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.RABBIT_ANALOG_TESTNET_CHAIN: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1434222),  # v1.3.0
+    ],
+    EthereumNetwork.TENET_TESTNET: [
+        ("0x0E19055869659F26f6b3b1Bd483A750E3c42A05E", 862908), # v1.3.0
     ],
     EthereumNetwork.CLOUDWALK_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 13743040),  # v1.3.0
