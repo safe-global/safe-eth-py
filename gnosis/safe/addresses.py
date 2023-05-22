2f114a34808faac50e66c873d043092a6e9e4576
Uxio Fuentefria <Uxio0@users.noreply.github.com>
2023-05-22 13:36:31 +0200

    Fix typo on safe addresses
    - Add a test so it doesn't happen again
    - Prepare a hotfix

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -188,8 +188,11 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
             22_123_383,
             "1.3.0+L2",
-        )(  # default singleton address
-            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 4_949_507, "1.3.0+L2"
+        ),  # default singleton address
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            4_949_507,
+            "1.3.0+L2",
         ),  # safe singleton address
         (
             "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
@@ -574,8 +577,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         (
             " 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             14_747_108,
-        )(  # v1.3.0 default singleton address
-            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 4_949_487
+        ),  # v1.3.0 default singleton address
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            4_949_487,
         ),  # v1.3.0 safe singleton address
     ],
     EthereumNetwork.MOONBEAM: [
