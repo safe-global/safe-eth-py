6a00b18818083b0df3cc9b3f2d84a5a65e811c0c
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-06-09 20:23:28 +0300

    feat: add MASTER_COPIES on Linea Goerli Testnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -472,6 +472,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 727470, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 727472, "1.3.0"),
     ],
+    EthereumNetwork.LINEA_GOERLI_TESTNET: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 363132, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 363135, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
