dfe15ae333105f4f2f9208092930ad07a89dc55d
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-06-14 14:03:20 +0300

    refactor: rename LINEA_GOERLI_TESTNET to LINEA_TESTNET

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -472,7 +472,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 727470, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 727472, "1.3.0"),
     ],
-    EthereumNetwork.LINEA_GOERLI_TESTNET: [
+    EthereumNetwork.LINEA_TESTNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 363132, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 363135, "1.3.0"),
     ],
@@ -787,7 +787,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.TENET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 727457),  # v1.3.0
     ],
-    EthereumNetwork.LINEA_GOERLI_TESTNET: [
+    EthereumNetwork.LINEA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 363118),  # v1.3.0
     ],
 }
