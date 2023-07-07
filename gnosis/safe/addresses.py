f14b5e92cca4f63ef9dd9f8a2685cae7ca26c690
iain nash <me@iain.in>
2023-07-07 11:52:17 -0400

    Add ZORA network

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -498,6 +498,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         )
     ],
+    EthereumNetwork.ZORA_NETWORK: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 11932, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 11934, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -829,5 +833,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
             739882,
         )
+    ],
+    EthereumNetwork.ZORA: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 11914),
     ],
 }
