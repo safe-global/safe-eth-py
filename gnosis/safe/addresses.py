ddd4bcb7e8a6fec08874e7ae31af784e4fe51b11
Uxio Fuentefria <uxio0@users.noreply.github.com>
2023-07-11 11:11:43 +0200

    Fix typos

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -505,6 +505,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.ZORA_NETWORK: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 11932, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 11934, "1.3.0"),
+    ],
     EthereumNetwork.ZKSYNC_ALPHA_TESTNET: [
         ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 8619879, "1.3.0+L2"),
         ("0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910", 8619884, "1.3.0"),
@@ -848,9 +849,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             739882,
         )
     ],
-    EthereumNetwork.ZORA: [
+    EthereumNetwork.ZORA_NETWORK: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 11914),  # v1.3.0
-    ]
+    ],
     EthereumNetwork.ZKSYNC_ALPHA_TESTNET: [
         ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 8619849),  # v1.3.0
     ],
