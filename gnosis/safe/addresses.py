9442614f4a99c01e48bc904a8f682bf1913d015e
msvstj <65896721+msvstj@users.noreply.github.com>
2023-07-19 18:18:59 +0900

    feat: add cascadia testnet contracts (#590)

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -522,6 +522,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 1511, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 1512, "1.3.0"),
     ],
+    EthereumNetwork.CASCADIA_TESTNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 1408599, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 1408613, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -871,5 +875,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.MANTLE: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1504),  # v1.3.0
+    ],
+    EthereumNetwork.CASCADIA_TESTNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1408580),  # v1.3.0
     ],
 }
