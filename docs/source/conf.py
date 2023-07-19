0c0125934a79e9e38450925c4d59f6f121bda3b0
Uxío <Uxio0@users.noreply.github.com>
2023-07-19 10:56:07 +0200

    Remove Uxio from copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 # -- Project information -----------------------------------------------------
 
 project = "Safe-eth-py"
-copyright = "2018-2023, Uxio"
+copyright = "2018-2023"
 author = "Uxio"
 
 
