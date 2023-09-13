c60aaf8494a079ce3b615fc6649548ad686d46e8
Major <90963895+Majorfi@users.noreply.github.com>
2023-09-13 10:16:23 +0200

    Update addresses.py
    feat: add missing GnosisSafeProxyFactory

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -638,7 +638,14 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 13736914),  # v1.3.0
     ],
     EthereumNetwork.POLYGON_ZKEVM: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 79000),  # v1.3.0
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            4460053,
+        ),  # v1.3.0 safe singleton address
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            79000,
+        ),  # v1.3.0 default singleton address
     ],
     EthereumNetwork.ARBITRUM_ONE: [
         (
