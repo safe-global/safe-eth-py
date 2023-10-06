52c108ed35302808e7524b33a2c68094872c5f91
HonzaDajc <jan.dajc@gmail.com>
2023-10-06 15:21:56 +0200

    Add Fantom

--- gnosis/eth/clients/etherscan_client.py
+++ gnosis/eth/clients/etherscan_client.py
@@ -48,6 +48,8 @@ class EtherscanClient:
         EthereumNetwork.NEON_EVM_MAINNET: "https://neonscan.org",
         EthereumNetwork.SEPOLIA: "https://sepolia.etherscan.io",
         EthereumNetwork.ZKSYNC_V2: "https://explorer.zksync.io/",
+        EthereumNetwork.FANTOM_OPERA: "https://ftmscan.com",
+        EthereumNetwork.FANTOM_TESTNET: "https://testnet.ftmscan.com/",
     }
 
     NETWORK_WITH_API_URL = {
@@ -76,6 +78,8 @@ class EtherscanClient:
         EthereumNetwork.NEON_EVM_MAINNET: "https://api.neonscan.org",
         EthereumNetwork.SEPOLIA: "https://api-sepolia.etherscan.io",
         EthereumNetwork.ZKSYNC_V2: "https://block-explorer-api.mainnet.zksync.io/",
+        EthereumNetwork.FANTOM_OPERA: "https://api.ftmscan.com",
+        EthereumNetwork.FANTOM_TESTNET: "https://api-testnet.ftmscan.com",
     }
     HTTP_HEADERS = {
         "User-Agent": "curl/7.77.0",
