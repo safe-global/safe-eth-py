c09d675d640c48de59b40017d40a2c420ff787f4
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-10-11 00:59:43 +0300

    feat: add Mantle etherscan api clients

--- gnosis/eth/clients/etherscan_client.py
+++ gnosis/eth/clients/etherscan_client.py
@@ -52,6 +52,8 @@ class EtherscanClient:
         EthereumNetwork.FANTOM_TESTNET: "https://testnet.ftmscan.com/",
         EthereumNetwork.LINEA: "https://www.lineascan.build",
         EthereumNetwork.LINEA_TESTNET: "https://goerli.lineascan.build",
+        EthereumNetwork.MANTLE: "https://explorer.mantle.xyz",
+        EthereumNetwork.MANTLE_TESTNET: "https://explorer.testnet.mantle.xyz",
     }
 
     NETWORK_WITH_API_URL = {
@@ -84,6 +86,8 @@ class EtherscanClient:
         EthereumNetwork.FANTOM_TESTNET: "https://api-testnet.ftmscan.com",
         EthereumNetwork.LINEA: "https://api.lineascan.build",
         EthereumNetwork.LINEA_TESTNET: "https://api-testnet.lineascan.build",
+        EthereumNetwork.MANTLE: "https://explorer.mantle.xyz",
+        EthereumNetwork.MANTLE_TESTNET: "https://explorer.testnet.mantle.xyz",
     }
     HTTP_HEADERS = {
         "User-Agent": "curl/7.77.0",
