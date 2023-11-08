445d01dff139eec863207555dfec8bdbc6a21b5d
cuonghx-gutech <cuonghx@gu-tech.com>
2023-11-08 16:52:07 +0700

    fix: lint

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -997,7 +997,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.RSK_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 2362232),  # v1.3.0
-    ],    
+    ],
     EthereumNetwork.JAPAN_OPEN_CHAIN_MAINNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 7709119),  # v1.3.0
     ],
