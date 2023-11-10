af4c4d832b3d5ec77fc3958fdd8d355b36ea9274
Uxio Fuentefria <uxio0@users.noreply.github.com>
2023-11-10 17:14:57 +0100

    Add AdvancedAdminSearchMixin for Django admin
    - Implement new AdvancedAdminSearchMixin
    - Don't convert every value to text for search, so DB indexes can be used
    - Change admin search from `iexact (=)`  to `exact (==)`
    - Related to https://github.com/safe-global/safe-transaction-service/issues/1710

--- gnosis/eth/django/admin.py
+++ gnosis/eth/django/admin.py
@@ -1,39 +1,84 @@
-from typing import Tuple
+from django.contrib.admin.utils import lookup_spawns_duplicates
+from django.core.exceptions import FieldDoesNotExist, ValidationError
+from django.db import models
+from django.db.models.constants import LOOKUP_SEP
+from django.utils.text import smart_split, unescape_string_literal
 
-from django.contrib import admin
-from django.core.exceptions import ValidationError
-from django.db.models import QuerySet
-from django.http import HttpRequest
 
-
-class BinarySearchAdmin(admin.ModelAdmin):
+class AdvancedAdminSearchMixin:
     """
-    Search inside binary fields, like EthereumAddressV2Field or Keccack256Field
+    Use database indexes when using exact search instead
+    of converting everything to text before searching
     """
 
-    def get_search_results(
-        self, request: HttpRequest, queryset: QuerySet, search_term: str
-    ) -> Tuple[QuerySet, bool]:
-        queryset, may_have_duplicates = super().get_search_results(
-            request, queryset, search_term
-        )
-        if search_term:
-            for search_field in self.get_search_fields(request):
+    def get_search_results(self, request, queryset, search_term):
+        """
+        Return a tuple containing a queryset to implement the search
+        and a boolean indicating if the results may contain duplicates.
+
+        This function was modified from Django original get_search_results
+        to allow `exact` search that uses database indexes
+        """
+
+        def construct_search(field_name):
+            if field_name.startswith("^"):
+                return "%s__istartswith" % field_name[1:]
+            elif field_name.startswith("=="):
+                return "%s__exact" % field_name[2:]
+            elif field_name.startswith("="):
+                return "%s__iexact" % field_name[1:]
+            elif field_name.startswith("@"):
+                return "%s__search" % field_name[1:]
+            # Use field_name if it includes a lookup.
+            opts = queryset.model._meta
+            lookup_fields = field_name.split(LOOKUP_SEP)
+            # Go through the fields, following all relations.
+            prev_field = None
+            for path_part in lookup_fields:
+                if path_part == "pk":
+                    path_part = opts.pk.name
                 try:
-                    if search_field.startswith("="):
-                        may_have_duplicates = True
-                        queryset |= self.model.objects.filter(
-                            **{search_field[1:]: search_term}
-                        )
-                    elif search_field.endswith("__icontains"):
-                        may_have_duplicates = True
-                        queryset |= self.model.objects.filter(
-                            **{
-                                search_field.replace("__icontains", "__contains"): [
-                                    search_term
-                                ]
-                            }
-                        )
-                except (ValidationError, ValueError):
-                    pass
+                    field = opts.get_field(path_part)
+                except FieldDoesNotExist:
+                    # Use valid query lookups.
+                    if prev_field and prev_field.get_lookup(path_part):
+                        return field_name
+                else:
+                    prev_field = field
+                    if hasattr(field, "path_infos"):
+                        # Update opts to follow the relation.
+                        opts = field.path_infos[-1].to_opts
+            # Otherwise, use the field with icontains.
+            return "%s__icontains" % field_name
+
+        may_have_duplicates = False
+        search_fields = self.get_search_fields(request)
+        if search_fields and search_term:
+            orm_lookups = [
+                construct_search(str(search_field)) for search_field in search_fields
+            ]
+            term_queries = []
+            for bit in smart_split(search_term):
+                if bit.startswith(('"', "'")) and bit[0] == bit[-1]:
+                    bit = unescape_string_literal(bit)
+
+                valid_queries = []
+                for orm_lookup in orm_lookups:
+                    try:
+                        # Check if query is valid (for example, not a number provided for an integer exact query)
+                        # This is the main difference comparing to Django official implementation
+                        queryset.filter(**{orm_lookup: bit})
+                        valid_queries.append((orm_lookup, bit))
+                    except (ValueError, ValidationError):
+                        pass
+                or_queries = models.Q.create(
+                    [valid_query for valid_query in valid_queries],
+                    connector=models.Q.OR,
+                )
+                term_queries.append(or_queries)
+            queryset = queryset.filter(models.Q.create(term_queries))
+            may_have_duplicates |= any(
+                lookup_spawns_duplicates(self.opts, search_spec)
+                for search_spec in orm_lookups
+            )
         return queryset, may_have_duplicates
