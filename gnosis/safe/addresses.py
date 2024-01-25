3623d08a92e354809a367b52aa51ab617820e56b
PanaW <pana.wang@outlook.com>
2024-01-25 14:22:30 +0800

    fix unclose bracket

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -679,6 +679,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             153642290,
             "1.4.1+L2",
         ),
+    ],
     EthereumNetwork.MANTA_PACIFIC_MAINNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 338471, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 338472, "1.3.0"),
@@ -1120,6 +1121,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.CONFLUX_ESPACE_TESTNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 153642185),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 66095530),  # v1.3.0
+    ],
     EthereumNetwork.MANTA_PACIFIC_MAINNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 338464),  # v1.3.0
     ],
