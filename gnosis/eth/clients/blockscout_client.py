ac3a8a899955cd6dece27f55ec9209ba1421053b
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-02-21 22:30:05 +0700

    feat: add blockscout client for unreal Testnet

--- gnosis/eth/clients/blockscout_client.py
+++ gnosis/eth/clients/blockscout_client.py
@@ -89,6 +89,7 @@ class BlockscoutClient:
         EthereumNetwork.MODE_TESTNET: "https://sepolia.explorer.mode.network/graphiql",
         EthereumNetwork.MANTLE_SEPOLIA_TESTNET: "https://explorer.sepolia.mantle.xyz/graphiql",
         EthereumNetwork.OP_SEPOLIA_TESTNET: "https://optimism-sepolia.blockscout.com/graphiql",
+        EthereumNetwork.UNREAL_TESTNET: "https://unreal.blockscout.com/graphiql",
     }
 
     def __init__(self, network: EthereumNetwork):
