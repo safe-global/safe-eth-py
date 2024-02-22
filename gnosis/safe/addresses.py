b791e12e67f575e1373f6dca88ba8687fe4ebdb9
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-02-22 18:48:47 +0700

    feat: add 1.3.0 safe contracts for Lisk Sepolia Testnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -896,6 +896,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 4552451, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 4552489, "1.3.0"),
     ],
+    EthereumNetwork.LISK_SEPOLIA_TESTNET: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 657757, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 657761, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1451,5 +1455,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.SEI_DEVNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 4552166),  # v1.3.0
+    ],
+    EthereumNetwork.LISK_SEPOLIA_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 657735),  # v1.3.0
     ],
 }
