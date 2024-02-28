28e0bccdbbac00aea17b8a3ccc701eb1921d9bb2
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-02-28 17:03:15 +0100

    Support batch requests on bundler RPC
    - Get UserOperation information and receipt in the same request

--- gnosis/eth/account_abstraction/bundler_client.py
+++ gnosis/eth/account_abstraction/bundler_client.py
@@ -1,13 +1,13 @@
 import logging
 from functools import lru_cache
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
 
 from eth_typing import ChecksumAddress, HexStr
 
 from gnosis.util.http import prepare_http_session
 
 from .exceptions import BundlerClientConnectionException, BundlerClientResponseException
-from .user_operation import UserOperation
+from .user_operation import UserOperation, UserOperationReceipt
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +20,7 @@ class BundlerClient:
     def __init__(
         self,
         url: str,
-        retry_count: int = 1,
+        retry_count: int = 0,
     ):
         self.url = url
         self.retry_count = retry_count
@@ -29,9 +29,11 @@ class BundlerClient:
     def __str__(self):
         return f"Bundler client at {self.url}"
 
-    def _do_request(self, payload: Dict[Any, Any]) -> Optional[Dict[str, Any]]:
+    def _do_request(
+        self, payload: Union[Dict[Any, Any], Sequence[Dict[Any, Any]]]
+    ) -> Union[Optional[Union[Dict[str, Any]]], Optional[List[Dict[str, Any]]]]:
         """
-        :param payload:
+        :param payload: Allows simple request or a batch request
         :return: Result of the request
         :raises BundlerClientConnectionException: If there's a problem connecting to the bundler
         :raises BundlerClientResponseException: If the request from the bundler contains an error
@@ -48,13 +50,52 @@ class BundlerClient:
                 f"Error connecting to bundler {self.url} : {response.status_code} {response.content}"
             )
 
-        response_json = response.json()
-        result = response_json.get("result")
-        if not result and "error" in response_json:
-            error_str = f'Bundler returned error for payload {payload} : {response_json["error"]}'
-            logger.warning(error_str)
-            raise BundlerClientResponseException(error_str)
-        return result
+        bundler_responses = response.json()
+        if not isinstance(bundler_responses, list):
+            bundler_responses = [bundler_responses]
+
+        results = []
+        for rpc_response in bundler_responses:
+            result = rpc_response.get("result")
+            if not result and "error" in rpc_response:
+                error_str = f'Bundler returned error for payload {payload} : {rpc_response["error"]}'
+                logger.warning(error_str)
+                raise BundlerClientResponseException(error_str)
+            results.append(result)
+
+        if not isinstance(payload, list):
+            return results[0]
+        return results
+
+    @staticmethod
+    def _parse_user_operation_receipt(
+        user_operation_receipt: Dict[str, Any]
+    ) -> UserOperationReceipt:
+        for field in ["nonce", "actualGasCost", "actualGasUsed"]:
+            user_operation_receipt[field] = int(user_operation_receipt[field], 16)
+        return user_operation_receipt
+
+    @staticmethod
+    def _get_user_operation_by_hash_payload(
+        user_operation_hash: HexStr, request_id: int = 1
+    ) -> Dict[str, Any]:
+        return {
+            "jsonrpc": "2.0",
+            "method": "eth_getUserOperationByHash",
+            "params": [user_operation_hash],
+            "id": request_id,
+        }
+
+    @staticmethod
+    def _get_user_operation_receipt_payload(
+        user_operation_hash: HexStr, request_id: int = 1
+    ) -> Dict[str, Any]:
+        return {
+            "jsonrpc": "2.0",
+            "method": "eth_getUserOperationReceipt",
+            "params": [user_operation_hash],
+            "id": request_id,
+        }
 
     @lru_cache(maxsize=1024)
     def get_user_operation_by_hash(
@@ -66,12 +107,7 @@ class BundlerClient:
         :param user_operation_hash:
         :return:
         """
-        payload = {
-            "jsonrpc": "2.0",
-            "method": "eth_getUserOperationByHash",
-            "params": [user_operation_hash],
-            "id": 1,
-        }
+        payload = self._get_user_operation_by_hash_payload(user_operation_hash)
         result = self._do_request(payload)
         return (
             UserOperation.from_bundler_response(user_operation_hash, result)
@@ -82,20 +118,32 @@ class BundlerClient:
     @lru_cache(maxsize=1024)
     def get_user_operation_receipt(
         self, user_operation_hash: HexStr
-    ) -> Optional[Dict[str, Any]]:
+    ) -> Optional[UserOperationReceipt]:
         """
         https://docs.alchemy.com/reference/eth-getuseroperationreceipt
 
         :param user_operation_hash:
         :return:
         """
-        payload = {
-            "jsonrpc": "2.0",
-            "method": "eth_getUserOperationReceipt",
-            "params": [user_operation_hash],
-            "id": 1,
-        }
-        return self._do_request(payload)
+        payload = self._get_user_operation_receipt_payload(user_operation_hash)
+        result = self._do_request(payload)
+        return self._parse_user_operation_receipt(result) if result else None
+
+    @lru_cache(maxsize=1024)
+    def get_user_operation_and_receipt(
+        self, user_operation_hash: HexStr
+    ) -> Optional[Tuple[UserOperation, UserOperationReceipt]]:
+        payload = [
+            self._get_user_operation_by_hash_payload(user_operation_hash, request_id=1),
+            self._get_user_operation_receipt_payload(user_operation_hash, request_id=2),
+        ]
+        result = self._do_request(payload)
+        if not (result and result[0]):
+            return None
+
+        return UserOperation.from_bundler_response(
+            user_operation_hash, result[0]
+        ), self._parse_user_operation_receipt(result[1])
 
     @lru_cache(maxsize=None)
     def supported_entry_points(self) -> List[ChecksumAddress]:
