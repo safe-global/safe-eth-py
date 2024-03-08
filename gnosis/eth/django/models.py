214a520d4bc1f8324614024887b786e5e95f7490
moisses89 <7888669+moisses89@users.noreply.github.com>
2024-03-08 09:24:30 +0100

    Add uint32Field

--- gnosis/eth/django/models.py
+++ gnosis/eth/django/models.py
@@ -109,19 +109,7 @@ class EthereumAddressV2Field(models.Field):
         return super().formfield(**defaults)
 
 
-class Uint256Field(models.DecimalField):
-    """
-    Field to store ethereum uint256 values. Uses Decimal db type without decimals to store
-    in the database, but retrieve as `int` instead of `Decimal` (https://docs.python.org/3/library/decimal.html)
-    """
-
-    description = _("Ethereum uint256 number")
-
-    def __init__(self, *args, **kwargs):
-        kwargs["max_digits"] = 78  # 2 ** 256 is 78 digits
-        kwargs["decimal_places"] = 0
-        super().__init__(*args, **kwargs)
-
+class UnsignedDecimal(models.DecimalField):
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         del kwargs["max_digits"]
@@ -146,7 +134,21 @@ class Uint256Field(models.DecimalField):
         return super().pre_save(model_instance, add)
 
 
-class Uint96Field(models.DecimalField):
+class Uint256Field(UnsignedDecimal):
+    """
+    Field to store ethereum uint256 values. Uses Decimal db type without decimals to store
+    in the database, but retrieve as `int` instead of `Decimal` (https://docs.python.org/3/library/decimal.html)
+    """
+
+    description = _("Ethereum uint256 number")
+
+    def __init__(self, *args, **kwargs):
+        kwargs["max_digits"] = 78  # 2 ** 256 is 78 digits
+        kwargs["decimal_places"] = 0
+        super().__init__(*args, **kwargs)
+
+
+class Uint96Field(UnsignedDecimal):
     """
     Field to store ethereum uint96 values. Uses Decimal db type without decimals to store
     in the database, but retrieve as `int` instead of `Decimal` (https://docs.python.org/3/library/decimal.html)
@@ -159,28 +161,19 @@ class Uint96Field(models.DecimalField):
         kwargs["decimal_places"] = 0
         super().__init__(*args, **kwargs)
 
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        del kwargs["max_digits"]
-        del kwargs["decimal_places"]
-        return name, path, args, kwargs
 
-    def from_db_value(self, value, expression, connection):
-        if value is None:
-            return value
-        return int(value)
+class Uint32Field(UnsignedDecimal):
+    """
+    Field to store ethereum uint32 values. Uses Decimal db type without decimals to store
+    in the database, but retrieve as `int` instead of `Decimal` (https://docs.python.org/3/library/decimal.html)
+    """
 
-    def pre_save(self, model_instance, add):
-        """
-        Override pre_save to ensure that field is unsigned before save it
-        :param model_instance:
-        :param add:
-        :return:
-        """
-        value = getattr(model_instance, self.attname)
-        if value is not None and value < 0:
-            raise ValidationError("Value must be an unsigned 96-bit integer")
-        return super().pre_save(model_instance, add)
+    description = _("Ethereum uint32 number")
+
+    def __init__(self, *args, **kwargs):
+        kwargs["max_digits"] = 10  # 2 ** 32 is 10 digits
+        kwargs["decimal_places"] = 0
+        super().__init__(*args, **kwargs)
 
 
 class HexField(models.CharField):
