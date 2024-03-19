dfd7a46ddcdf33d4c8c77a2e5e5d055889176b20
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-03-19 12:04:46 +0100

    Support failed user operations

--- gnosis/eth/account_abstraction/user_operation_receipt.py
+++ gnosis/eth/account_abstraction/user_operation_receipt.py
@@ -3,13 +3,14 @@ from typing import Any, Dict, List, Optional
 
 from eth_typing import ChecksumAddress
 from hexbytes import HexBytes
-from web3 import Web3
 from web3.types import LogReceipt
 
 from gnosis.eth.account_abstraction.constants import (
     DEPOSIT_EVENT_TOPIC,
+    EXECUTION_FROM_MODULE_FAILURE_TOPIC,
     EXECUTION_FROM_MODULE_SUCCESS_TOPIC,
 )
+from gnosis.eth.utils import fast_to_checksum_address
 
 
 @dataclasses.dataclass(eq=True, frozen=True)
@@ -52,8 +53,8 @@ class UserOperationReceipt:
             if (
                 len(log["topics"]) == 2
                 and HexBytes(log["topics"][0]) == DEPOSIT_EVENT_TOPIC
-                and Web3.to_checksum_address(log["address"]) == self.entry_point
-                and Web3.to_checksum_address(log["topics"][1][-40:]) == self.sender
+                and fast_to_checksum_address(log["address"]) == self.entry_point
+                and fast_to_checksum_address(log["topics"][1][-40:]) == self.sender
             ):
                 deposited += int(log["data"], 16)
         return deposited
@@ -67,8 +68,12 @@ class UserOperationReceipt:
         for log in reversed(self.logs):
             if (
                 len(log["topics"]) == 2
-                and HexBytes(log["topics"][0]) == EXECUTION_FROM_MODULE_SUCCESS_TOPIC
-                and Web3.to_checksum_address(log["address"]) == self.sender
+                and HexBytes(log["topics"][0])
+                in (
+                    EXECUTION_FROM_MODULE_SUCCESS_TOPIC,
+                    EXECUTION_FROM_MODULE_FAILURE_TOPIC,
+                )
+                and fast_to_checksum_address(log["address"]) == self.sender
             ):
-                return Web3.to_checksum_address(log["topics"][1][-40:])
+                return fast_to_checksum_address(log["topics"][1][-40:])
         return None
