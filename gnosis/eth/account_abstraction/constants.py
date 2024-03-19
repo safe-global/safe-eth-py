a7599a50f2ac84a3b8e012a9c728c2ce488636c2
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-03-19 12:04:46 +0100

    Support failed user operations

--- gnosis/eth/account_abstraction/constants.py
+++ gnosis/eth/account_abstraction/constants.py
@@ -6,8 +6,13 @@ DEPOSIT_EVENT_TOPIC = HexBytes(
     "0x2da466a7b24304f47e87fa2e1e5a81b9831ce54fec19055ce277ca2f39ba42c4"
 )
 
-# Safe > 1.4.1 event
+# Safe (L2 not required) >= 1.4.1 events
 # ExecutionFromModuleSuccess (index_topic_1 address module)
 EXECUTION_FROM_MODULE_SUCCESS_TOPIC = HexBytes(
     "0x6895c13664aa4f67288b25d7a21d7aaa34916e355fb9b6fae0a139a9085becb8"
+)
+
+# ExecutionFromModuleFailure (index_topic_1 address module)
+EXECUTION_FROM_MODULE_FAILURE_TOPIC = HexBytes(
+    "0xacd2c8702804128fdb0db2bb49f6d127dd0181c13fd45dbfe16de0930e2bd375"
 )
