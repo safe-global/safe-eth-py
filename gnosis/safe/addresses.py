7cb5c1689f13ca8510c316d558570abfd3bda4cf
Ali <ali.salloum.gh@gmail.com>
2024-03-22 14:13:56 +0300

    fixed linting

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -868,7 +868,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.LISK_SEPOLIA_TESTNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 657757, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 657761, "1.3.0"),
-    ],  
+    ],
     EthereumNetwork.BOTANIX_TESTNET: [
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 165429, "1.4.1+L2"),
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 165430, "1.4.1"),
@@ -1421,7 +1421,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 657735),  # v1.3.0
     ],
     EthereumNetwork.BOTANIX_TESTNET: [
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 165422), # v1.4.1
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 165422),  # v1.4.1
     ],
     EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1087898),  # v1.3.0
