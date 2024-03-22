2702d507a88a33338fc39db63061621947336adc
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-03-22 17:36:44 +0700

    feat: add Blast Sepolia support (#791)
    * feat: add Blast Sepolia chain id
    
    * feat: add 1.3.0 safe contracts for Blast Sepolia
    
    * feat: add blockscout client for Blast Sepolia
    
    * feat: update etherscan client for Blast Sepolia
    
    ---------
    
    Co-authored-by: nick8319 <zhiltsov.nick@gmail.com>

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -869,6 +869,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 657757, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 657761, "1.3.0"),
     ],
+    EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1087958, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1087964, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1411,5 +1415,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.LISK_SEPOLIA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 657735),  # v1.3.0
+    ],
+    EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1087898),  # v1.3.0
     ],
 }
