d12a8e24671c7e69df364e041e3fd7505dd777fd
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-03-30 15:18:02 +0700

    feat: add 1.3.0 contracts for Astar zKyoto

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1035,6 +1035,28 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0",
         ),  # default singleton address
     ],
+    EthereumNetwork.ASTAR_ZKYOTO: [
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            914726,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            1026758,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            914732,
+            "1.3.0",
+        ),  # default singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            1026763,
+            "1.3.0",
+        ),  # safe singleton address
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1663,5 +1685,15 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             132405,
         ),  # v1.3.0  Default singleton address
+    ],
+    EthereumNetwork.ASTAR_ZKYOTO: [
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            1026735,
+        ),  # v1.3.0 safe singleton address
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            914700,
+        ),  # v1.3.0 default singleton address
     ],
 }
