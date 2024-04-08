324a5d0e7f9c16397b2a4e828e699b1c3e657d96
Moisés <7888669+moisses89@users.noreply.github.com>
2024-04-08 12:53:08 +0200

    Remove api path from url

--- gnosis/eth/clients/etherscan_client.py
+++ gnosis/eth/clients/etherscan_client.py
@@ -103,7 +103,7 @@ class EtherscanClient:
         EthereumNetwork.KROMA_SEPOLIA: "https://api-sepolia.kromascan.com",
         EthereumNetwork.BLAST_SEPOLIA_TESTNET: "https://api-sepolia.blastscan.io",
         EthereumNetwork.FRAXTAL_MAINNET: "https://api.fraxscan.com",
-        EthereumNetwork.BASE: "https://api.basescan.org/api",
+        EthereumNetwork.BASE: "https://api.basescan.org",
     }
     HTTP_HEADERS = {
         "User-Agent": "curl/7.77.0",
