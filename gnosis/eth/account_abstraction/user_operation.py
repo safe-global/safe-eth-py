25e33713443f7472a93f6c7a24a4cf44944bf8b0
Uxío <Uxio0@users.noreply.github.com>
2024-05-07 16:29:31 +0200

    4337: Support 0.7 Entrypoint (#948)
    - Closes #785

--- gnosis/eth/account_abstraction/user_operation.py
+++ gnosis/eth/account_abstraction/user_operation.py
@@ -21,7 +21,7 @@ class UserOperation:
     """
     EIP4337 UserOperation for Entrypoint v0.6
 
-    https://github.com/eth-infinitism/account-abstraction/blob/v0.6.0
+    https://github.com/eth-infinitism/account-abstraction/blob/v0.6.0/contracts/interfaces/UserOperation.sol
     """
 
     user_operation_hash: bytes
@@ -44,27 +44,65 @@ class UserOperation:
         cls,
         user_operation_hash: Union[HexStr, bytes],
         user_operation_response: Dict[str, Any],
-    ) -> "UserOperation":
-        return cls(
-            HexBytes(user_operation_hash),
-            ChecksumAddress(user_operation_response["userOperation"]["sender"]),
-            int(user_operation_response["userOperation"]["nonce"], 16),
-            HexBytes(user_operation_response["userOperation"]["initCode"]),
-            HexBytes(user_operation_response["userOperation"]["callData"]),
-            int(user_operation_response["userOperation"]["callGasLimit"], 16),
-            int(user_operation_response["userOperation"]["verificationGasLimit"], 16),
-            int(user_operation_response["userOperation"]["preVerificationGas"], 16),
-            int(user_operation_response["userOperation"]["maxFeePerGas"], 16),
-            int(user_operation_response["userOperation"]["maxPriorityFeePerGas"], 16),
-            HexBytes(user_operation_response["userOperation"]["paymasterAndData"]),
-            HexBytes(user_operation_response["userOperation"]["signature"]),
-            ChecksumAddress(user_operation_response["entryPoint"]),
-            metadata=UserOperationMetadata(
-                HexBytes(user_operation_response["transactionHash"]),
-                HexBytes(user_operation_response["blockHash"]),
-                int(user_operation_response["blockNumber"], 16),
-            ),
+    ) -> Union["UserOperation", "UserOperationV07"]:
+        user_operation = user_operation_response["userOperation"]
+        metadata = UserOperationMetadata(
+            HexBytes(user_operation_response["transactionHash"]),
+            HexBytes(user_operation_response["blockHash"]),
+            int(user_operation_response["blockNumber"], 16),
         )
+        if "initCode" in user_operation:
+            return cls(
+                HexBytes(user_operation_hash),
+                ChecksumAddress(user_operation["sender"]),
+                int(user_operation["nonce"], 16),
+                HexBytes(user_operation["initCode"]),
+                HexBytes(user_operation["callData"]),
+                int(user_operation["callGasLimit"], 16),
+                int(user_operation["verificationGasLimit"], 16),
+                int(user_operation["preVerificationGas"], 16),
+                int(user_operation["maxFeePerGas"], 16),
+                int(user_operation["maxPriorityFeePerGas"], 16),
+                HexBytes(user_operation["paymasterAndData"]),
+                HexBytes(user_operation["signature"]),
+                ChecksumAddress(user_operation_response["entryPoint"]),
+                metadata=metadata,
+            )
+        else:
+            if paymaster := user_operation.get("paymaster"):
+                # Paymaster parameters are optional
+                paymaster_verification_gas_limit = int(
+                    user_operation["paymasterVerificationGasLimit"], 16
+                )
+                paymaster_post_op_gas_limit = int(
+                    user_operation["paymasterPostOpGasLimit"], 16
+                )
+                paymaster_data = HexBytes(user_operation["paymasterData"])
+            else:
+                paymaster_verification_gas_limit = None
+                paymaster_post_op_gas_limit = None
+                paymaster_data = None
+
+            return UserOperationV07(
+                HexBytes(user_operation_hash),
+                ChecksumAddress(user_operation["sender"]),
+                int(user_operation["nonce"], 16),
+                ChecksumAddress(user_operation["factory"]),
+                HexBytes(user_operation["factoryData"]),
+                HexBytes(user_operation["callData"]),
+                int(user_operation["callGasLimit"], 16),
+                int(user_operation["verificationGasLimit"], 16),
+                int(user_operation["preVerificationGas"], 16),
+                int(user_operation["maxPriorityFeePerGas"], 16),
+                int(user_operation["maxFeePerGas"], 16),
+                HexBytes(user_operation["signature"]),
+                ChecksumAddress(user_operation_response["entryPoint"]),
+                paymaster_verification_gas_limit,
+                paymaster_post_op_gas_limit,
+                paymaster,
+                paymaster_data,
+                metadata=metadata,
+            )
 
     def __str__(self):
         return f"User Operation sender={self.sender} nonce={self.nonce} hash={self.user_operation_hash.hex()}"
@@ -107,6 +145,96 @@ class UserOperation:
                 self.pre_verification_gas,
                 self.max_fee_per_gas,
                 self.max_priority_fee_per_gas,
+                hash_paymaster_and_data,
+            ],
+        )
+        return fast_keccak(
+            abi_encode(
+                ["bytes32", "address", "uint256"],
+                [fast_keccak(user_operation_encoded), self.entry_point, chain_id],
+            )
+        )
+
+
+@dataclasses.dataclass(eq=True, frozen=True)
+class UserOperationV07:
+    """
+    EIP4337 UserOperation for Entrypoint v0.7
+
+    https://github.com/eth-infinitism/account-abstraction/blob/v0.7.0/contracts/interfaces/PackedUserOperation.sol
+    """
+
+    user_operation_hash: bytes
+    sender: ChecksumAddress
+    nonce: int
+    factory: ChecksumAddress
+    factory_data: bytes
+    call_data: bytes
+    call_gas_limit: int
+    verification_gas_limit: int
+    pre_verification_gas: int
+    max_priority_fee_per_gas: int
+    max_fee_per_gas: int
+    signature: bytes
+    entry_point: ChecksumAddress
+    paymaster_verification_gas_limit: Optional[int] = None
+    paymaster_post_op_gas_limit: Optional[int] = None
+    paymaster: Optional[bytes] = None
+    paymaster_data: Optional[bytes] = None
+    metadata: Optional[UserOperationMetadata] = None
+
+    @property
+    def account_gas_limits(self) -> bytes:
+        """
+        :return:Account Gas Limits is a `bytes32` in Solidity, first `bytes16` `verification_gas_limit` and then `call_gas_limit`
+        """
+        return HexBytes(self.verification_gas_limit).rjust(16, b"\x00") + HexBytes(
+            self.call_gas_limit
+        ).rjust(16, b"\x00")
+
+    @property
+    def gas_fees(self) -> bytes:
+        """
+        :return: Gas Fees is a `bytes32` in Solidity, first `bytes16` `verification_gas_limit` and then `call_gas_limit`
+        """
+        return HexBytes(self.max_priority_fee_per_gas).rjust(16, b"\x00") + HexBytes(
+            self.max_fee_per_gas
+        ).rjust(16, b"\x00")
+
+    @property
+    def paymaster_and_data(self) -> bytes:
+        if not self.paymaster:
+            return b""
+        return (
+            HexBytes(self.paymaster).rjust(20, b"\x00")
+            + HexBytes(self.paymaster_verification_gas_limit).rjust(16, b"\x00")
+            + HexBytes(self.paymaster_post_op_gas_limit).rjust(16, b"\x00")
+            + HexBytes(self.paymaster_data)
+        )
+
+    def calculate_user_operation_hash(self, chain_id: int) -> bytes:
+        hash_init_code = fast_keccak(HexBytes(self.factory) + self.factory_data)
+        hash_call_data = fast_keccak(self.call_data)
+        hash_paymaster_and_data = fast_keccak(self.paymaster_and_data)
+        user_operation_encoded = abi_encode(
+            [
+                "address",
+                "uint256",
+                "bytes32",
+                "bytes32",
+                "bytes32",
+                "uint256",
+                "bytes32",
+                "bytes32",
+            ],
+            [
+                self.sender,
+                self.nonce,
+                hash_init_code,
+                hash_call_data,
+                self.account_gas_limits,
+                self.pre_verification_gas,
+                self.gas_fees,
                 hash_paymaster_and_data,
             ],
         )
