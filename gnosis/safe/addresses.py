3b13975b3dc17970c820544d38efdfccaf09fd92
Felipe Alvarado <felipe@safe.global>
2024-05-31 14:02:49 +0200

    Fix Optimism address

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -279,6 +279,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             173751,
             "1.3.0",
         ),  # safe singleton address
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 111449608, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            113995630,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
     ],
     EthereumNetwork.BOBA_BNB_MAINNET: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 22284, "1.3.0+L2"),
@@ -1133,14 +1139,6 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 253322, "1.3.0"),  # v1.3.0
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 253199, "1.3.0+L2"),  # v1.3.0+L2
     ],
-    EthereumNetwork.OP_MAINNET: [
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 111449608, "1.4.1"),  # v1.4.1
-        (
-            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
-            113995630,
-            "1.4.1+L2",
-        ),  # v1.4.1+L2
-    ],
     EthereumNetwork.AMOY: [
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 6006133, "1.4.1"),  # v1.4.1
         (
@@ -1304,6 +1302,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
             173709,
         ),  # v1.3.0 safe singleton address
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 105342078),  # v1.4.1
     ],
     EthereumNetwork.BOBA_BNB_MAINNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 22831),  # v1.3.0
@@ -1828,9 +1827,6 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.TAIKO_HEKLA_L2: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 158794),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 253167),  # v1.3.0
-    ],
-    EthereumNetwork.OP_MAINNET: [
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 105342078),  # v1.4.1
     ],
     EthereumNetwork.AMOY: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 6005942),  # v1.4.1
