262e81cd12f97e65c4262791f6d9609d75a0a1d0
Wong Tse Jian <wongtsejian@gmail.com>
2024-06-04 11:19:06 +0800

    add 1.4.1 proxy factory address

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1318,6 +1318,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.AVALANCHE_C_CHAIN: [
         (
+            "0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 
+            37031436,
+        ),  # v1.4.1
+        (
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             14_747_108,
         ),  # v1.3.0 default singleton address
