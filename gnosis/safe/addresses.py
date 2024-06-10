3e8c9f462cda53e75c1f6f0f3877c97192202153
Felipe Alvarado <felipe@safe.global>
2024-06-10 10:32:58 +0200

    Add missing comment

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -611,7 +611,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
             2807830,
             "1.4.1+L2",
-        ),
+        ),  # v1.4.1+L2
     ],
     EthereumNetwork.ASTAR: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1106426, "1.3.0+L2"),
