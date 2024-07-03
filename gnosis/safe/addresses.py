0a39091cf8dd0dd78187d205e12375957f159c5a
Den Smalonski <dzianis.smalonski@protofire.io>
2024-07-03 13:20:58 +0200

    feat: add bob network

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1567,6 +1567,16 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.4.1+L2",
         ),  # v1.4.1+L2
     ],
+    EthereumNetwork.BOB: [
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            199948,
+        ),  # v1.3.0 safe singleton address
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            200468,
+        ),  # v1.3.0 default singleton address
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -2352,5 +2362,27 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.F_XCORE_MAINNET_NETWORK: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 16088791),  # v1.4.1
+    ],
+    EthereumNetwork.BOB: [
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            200503,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            0,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            200509,
+            "1.3.0",
+        ),  # default singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            0,
+            "1.3.0",
+        ),  # safe singleton address
     ],
 }
