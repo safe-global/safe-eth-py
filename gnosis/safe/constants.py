a2b015a7364d6b428945418eed6ce41fdb3b5d98
Uxio Fuentefria <uxio0@users.noreply.github.com>
2024-07-16 15:28:39 +0200

    Refactor `SafeCreate2TxBuilder`
    - Improve docs and typing
    - Closes #1206

---
+++ gnosis/safe/constants.py
@@ -0,0 +1,3 @@
+# Gas required to transfer a regular token. Assume the worst scenario with a regular token transfer without storage
+# initialized (payment_receiver no previous owner of token)
+TOKEN_TRANSFER_GAS = 60_000
