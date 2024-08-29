091dee598b2bf4be7823c8bd2e9dfc09cfc82adc
Felipe Alvarado <felipe@safe.global>
2024-08-29 11:10:41 +0200

    Add mypy to run_tests and CI

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-# Postgresql and ganache-cli must be running for the tests
 
+# Llama a mypy y detiene el script si falla
+mypy . || exit 1
+
+# Postgresql and ganache-cli must be running for the tests
 docker-compose up -d db ganache
+
 sleep 3
 
 # python manage.py test --settings=config.settings.test
