3f0e6819e67db1430d2f6d9724b0afab9bf8feab
Felipe Alvarado <felipe@safe.global>
2024-09-10 12:27:56 +0200

    Remove duplicate mypy checks

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
-# Execute mypy and stop script if it fails
-mypy . || exit 1
-
 # Postgresql and ganache-cli must be running for the tests
-docker-compose up -d db ganache
 
+docker-compose up -d db ganache
 sleep 3
 
 # python manage.py test --settings=config.settings.test
