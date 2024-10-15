b03e67c9fb08e178c5d9a3384f950c41316941a9
moisses89 <7888669+moisses89@users.noreply.github.com>
2024-10-15 15:15:18 +0200

    Fix typying proposer can be None

--- safe_eth/safe/api/transaction_service_api/transaction_service_tx.py
+++ safe_eth/safe/api/transaction_service_api/transaction_service_tx.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from eth_typing import ChecksumAddress
 
 from safe_eth.safe import SafeTx
 
 
 class TransactionServiceTx(SafeTx):
-    def __init__(self, proposer: ChecksumAddress, *args, **kwargs):
+    def __init__(self, proposer: Optional[ChecksumAddress], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.proposer = proposer
