219cb6ac74d05c9dd40c073765dc723d0f47822b
Felipe Alvarado <felipe@safe.global>
2024-12-04 09:37:02 +0100

    Add master copies addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1982,6 +1982,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.SWELL_NETWORK: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 261408, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 261401, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 0, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 0, "1.3.0+L2"),  # v1.3.0+L2
     ],
 }
 
