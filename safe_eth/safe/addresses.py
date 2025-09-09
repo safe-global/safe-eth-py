6c2e0b5be2f8e96da536e7278223f1e4507d40ef
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-09-09 11:13:20 +0200

    Fix 1.4.1 block (GENESIS)

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2830,8 +2830,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 64428, "1.3.0+L2"),  # v1.3.0+L2
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 63827, "1.3.0"),  # v1.3.0
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 63824, "1.3.0+L2"),  # v1.3.0+L2
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 131582, "1.4.1"),  # v1.4.1
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 131666, "1.4.1+L2"),  # v1.4.1+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 0, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 0, "1.4.1+L2"),  # v1.4.1+L2
     ],
 }
 
@@ -4086,7 +4086,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.PLASMA_MAINNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 64075),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 63796),  # v1.3.0
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 131582),  # v1.4.1
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 0),  # v1.4.1
     ],
 }
 
