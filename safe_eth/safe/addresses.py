40579ee98b0137d101d107dc3f3bb2cd2443c550
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-10-22 11:20:46 +0200

    Add missing addresses (#2099)

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3061,6 +3061,18 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             8633486,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 8634117, "1.3.0"),  # v1.3.0
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            8634115,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 8634152, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            8634155,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
     ],
     EthereumNetwork.INCENTIV: [
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 223237, "1.3.0"),  # v1.3.0
@@ -4511,6 +4523,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.MANTRACHAIN_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 8633473),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 8634102),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 8634138),  # v1.4.1
     ],
     EthereumNetwork.INCENTIV: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 223220),  # v1.3.0
