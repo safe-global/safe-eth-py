eca503bf47ff5416b08089b1644bc6c79134c9e2
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-11-05 16:18:11 +0100

    Add STABLE_MAINNET addresses (#2118)

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3218,6 +3218,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 20705, "1.4.1"),  # v1.4.1
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 20706, "1.4.1+L2"),  # v1.4.1+L2
     ],
+    EthereumNetwork.STABLE_MAINNET: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 652488, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 652487, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 648576, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 648577, "1.4.1+L2"),  # v1.4.1+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -4594,6 +4600,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.SOPHON_ZKSYNC_OS_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 20052),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 20697),  # v1.4.1
+    ],
+    EthereumNetwork.STABLE_MAINNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 652480),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 648567),  # v1.4.1
     ],
 }
 
