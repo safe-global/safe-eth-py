96ac1642201ad9022bf53876281ce1dd6ea7204d
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-11-21 10:51:20 +0100

    Add Megaeth 1.3.0 155 addresses (#2175)

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3269,6 +3269,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 592337, "1.4.1+L2"),  # v1.4.1+L2
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 591953, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 591969, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 854088, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 854090, "1.3.0+L2"),  # v1.3.0+L2
     ],
 }
 
@@ -4675,6 +4677,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.MEGAETH_MAINNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 592359),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 592012),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 854086),  # v1.3.0
     ],
 }
 
