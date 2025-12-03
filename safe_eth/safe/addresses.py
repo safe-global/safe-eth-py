54bf6feba58474845d38300ed0ea419db3301753
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-12-03 12:28:04 +0100

    Fix network enum name

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3320,7 +3320,11 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     ],
     EthereumNetwork.CHILIZ_CHAIN: [
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 18461551, "1.4.1"),  # v1.4.1
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 18461553, "1.4.1+L2"),  # v1.4.1+L2
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            18461553,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
     ],
 }
 
