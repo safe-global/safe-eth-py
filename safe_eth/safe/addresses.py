22e508f775f923dc3cb10e1590afaefd78291219
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2026-04-13 10:30:35 +0200

    Add 1.4.1 master copy address

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3548,6 +3548,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             32948517,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 0, "1.4.1"),  # v1.4.1
         (
             "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
             11115061,
