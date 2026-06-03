42c4fec1995e0dc9b67885ff6409d9f8183fab3a
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-06-03 11:54:46 +0200

    Add addresses 1.5.0 for chain MIZUHIKI_TESTNET_AWAJI (#2521)
    * Add new master copy address 0xFf51A5898e281Db6DfC7855790607438dF2ca44b
    
    * Add new master copy address 0xEdd160fEBBD92E350D4D398fb636302fccd67C7e
    
    * Add new proxy address 0x14F2982D601c9458F93bd70B218933A6f8165e7b
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3876,6 +3876,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 634995, "1.4.1+L2"),  # v1.4.1+L2
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 648806, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 647641, "1.3.0+L2"),  # v1.3.0+L2
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 664473, "1.5.0"),  # v1.5.0
+        ("0xEdd160fEBBD92E350D4D398fb636302fccd67C7e", 664475, "1.5.0+L2"),  # v1.5.0+L2
     ],
 }
 
@@ -5437,6 +5439,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 638944),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 634348),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 647522),  # v1.3.0
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 663401),  # v1.5.0
     ],
 }
 
