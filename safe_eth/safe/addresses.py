a960301bf87ea14728157fdc94668461d730ce01
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-06-10 16:42:26 +0200

    Add addresses 1.3.0 for chain KAIA_MAINNET (#2532)
    * Add new master copy address 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552
    
    * Add new master copy address 0x3E5c63644E683549055b9Be8653de26E0B4CD36E
    
    * Add new proxy address 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -495,6 +495,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     ],
     EthereumNetwork.KAIA_MAINNET: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 93507490, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 110190348, "1.3.0"),  # v1.3.0
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            196021570,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
     ],
     EthereumNetwork.MILKOMEDA_A1_TESTNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 796, "1.3.0+L2"),
@@ -4120,6 +4126,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.KAIA_MAINNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 93506870),  # v1.3.0
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 110190257),  # v1.3.0
     ],
     EthereumNetwork.MILKOMEDA_A1_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 789),  # v1.3.0
