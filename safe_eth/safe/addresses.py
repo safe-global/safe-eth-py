a37c515b5b2c1bc847b73a365d68d967b801cd14
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-06-26 16:50:14 +0200

    Add addresses 1.5.0 for chain JAPAN_OPEN_CHAIN_MAINNET (#2550)
    * Add new master copy address 0xFf51A5898e281Db6DfC7855790607438dF2ca44b
    
    * Add new master copy address 0xEdd160fEBBD92E350D4D398fb636302fccd67C7e
    
    * Add new proxy address 0x14F2982D601c9458F93bd70B218933A6f8165e7b
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1161,6 +1161,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             22599484,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 14598435, "1.5.0"),  # v1.5.0
+        (
+            "0xEdd160fEBBD92E350D4D398fb636302fccd67C7e",
+            14598436,
+            "1.5.0+L2",
+        ),  # v1.5.0+L2
     ],
     EthereumNetwork.JAPAN_OPEN_CHAIN_TESTNET: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 1315570, "1.3.0+L2"),
@@ -4444,6 +4450,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 7709119),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 13877066),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 22599470),  # v1.3.0
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 14598425),  # v1.5.0
     ],
     EthereumNetwork.JAPAN_OPEN_CHAIN_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1315556),  # v1.3.0
