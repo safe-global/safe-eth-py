29124ec42541bc338256f6c928cd26af0ffd3114
Felipe Alvarado <felipe@safe.global>
2026-06-29 11:15:40 +0200

    Add addresses 1.5.0 for chain SEI_TESTNET (#2559)

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3335,6 +3335,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             188492825,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 193005199, "1.5.0"),  # v1.5.0
+        (
+            "0xEdd160fEBBD92E350D4D398fb636302fccd67C7e",
+            243163971,
+            "1.5.0+L2",
+        ),  # v1.5.0+L2
     ],
     EthereumNetwork.MANTRACHAIN_TESTNET: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 8633488, "1.3.0"),  # v1.3.0
@@ -5340,6 +5346,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 188493669),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 188493154),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 188492754),  # v1.3.0
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 193005188),  # v1.5.0
     ],
     EthereumNetwork.MANTRACHAIN_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 8633473),  # v1.3.0
