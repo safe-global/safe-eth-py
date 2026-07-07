5df946b81e617751e316775469584bfb8270d533
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-07-07 09:26:25 +0200

    Add addresses 1.3.0 for chain BOT_CHAIN_MAINNET (#2569)
    * Add new chain 677
    
    * Add new master copy address 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552
    
    * Add new master copy address 0x3E5c63644E683549055b9Be8653de26E0B4CD36E
    
    * Add new proxy address 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3978,6 +3978,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         ),  # v1.3.0+L2
     ],
+    EthereumNetwork.BOT_CHAIN_MAINNET: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 15132822, "1.3.0"),  # v1.3.0
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            15132827,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -5557,6 +5565,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.SEISMIC_TESTNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 11481786),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 11481510),  # v1.3.0
+    ],
+    EthereumNetwork.BOT_CHAIN_MAINNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 15132814),  # v1.3.0
     ],
 }
 
