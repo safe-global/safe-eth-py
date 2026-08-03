a84e169f3ab232cff6caae670d948a5492dbced8
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-03 16:15:06 +0200

    Add addresses 1.4.1 for chain GRAVITY (#2634)
    * Add new chain 127001
    
    * Add new explorer client URL: https://api.mainnet-explorer.gravity.xyz/api/v2/
    
    * Add new master copy address 0x41675C099F32341bf84BFc5382aF534df5C7461a
    
    * Add new master copy address 0x29fcB43b46531BcA003ddC8FCB67FFE91900C762
    
    * Add new proxy address 0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -4028,6 +4028,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 0, "1.3.0"),  # v1.3.0
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 0, "1.3.0+L2"),  # v1.3.0+L2
     ],
+    EthereumNetwork.GRAVITY: [
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 14262239, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            14262239,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -5620,6 +5628,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2509796),  # v1.4.1
         ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 45035),  # v1.5.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2509742),  # v1.3.0
+    ],
+    EthereumNetwork.GRAVITY: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 14262239),  # v1.4.1
     ],
 }
 
