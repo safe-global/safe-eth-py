899baef4795a3f75f824b3f99ed67ce50a5f202b
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-04 10:10:55 +0200

    Add addresses 1.5.0 for chain HYPEREVM (#2642)
    * Add new master copy address 0xFf51A5898e281Db6DfC7855790607438dF2ca44b
    
    * Add new master copy address 0xEdd160fEBBD92E350D4D398fb636302fccd67C7e
    
    * Add new proxy address 0x14F2982D601c9458F93bd70B218933A6f8165e7b
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3601,6 +3601,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 81553, "1.3.0+L2"),  # v1.3.0+L2
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 21072, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 21041, "1.3.0+L2"),  # v1.3.0+L2
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 11572422, "1.5.0"),  # v1.5.0
+        (
+            "0xEdd160fEBBD92E350D4D398fb636302fccd67C7e",
+            14617265,
+            "1.5.0+L2",
+        ),  # v1.5.0+L2
     ],
     EthereumNetwork.HPP_MAINNET: [
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 13700, "1.4.1"),  # v1.4.1
@@ -5533,6 +5539,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 60597),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 19163),  # v1.3.0
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 19591),  # v1.3.0
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 11567819),  # v1.5.0
     ],
     EthereumNetwork.HPP_MAINNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 13691),  # v1.4.1
