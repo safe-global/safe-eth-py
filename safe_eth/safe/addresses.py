69fbec9087258e2a5f6e6186de47836dce3a1a7e
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-04 15:14:53 +0200

    Add addresses 1.5.0 for chain LINEA (#2660)
    * Add new master copy address 0xFf51A5898e281Db6DfC7855790607438dF2ca44b
    
    * Add new master copy address 0xEdd160fEBBD92E350D4D398fb636302fccd67C7e
    
    * Add new proxy address 0x14F2982D601c9458F93bd70B218933A6f8165e7b
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1090,6 +1090,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             1583049,
             "1.4.1+L2",
         ),  # v1.4.1+L2
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 22251699, "1.5.0"),  # v1.5.0
+        (
+            "0xEdd160fEBBD92E350D4D398fb636302fccd67C7e",
+            28757295,
+            "1.5.0+L2",
+        ),  # v1.5.0+L2
     ],
     EthereumNetwork.NEON_EVM_DEVNET: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 205147021, "1.3.0+L2"),
@@ -4649,6 +4655,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             10,
         ),  # v1.3.0  Default singleton address
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 1582959),  # v1.4.1
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 22251693),  # v1.5.0
     ],
     EthereumNetwork.NEON_EVM_DEVNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 205146874),  # v1.3.0
