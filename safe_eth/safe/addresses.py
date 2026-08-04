3c3e058ed055b952f872e13bf3894bf3eb52388b
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-04 09:06:30 +0200

    Add addresses 1.5.0 for chain POLYGON_MAINNET (#2638)
    * Add new master copy address 0xFf51A5898e281Db6DfC7855790607438dF2ca44b
    
    * Add new master copy address 0xEdd160fEBBD92E350D4D398fb636302fccd67C7e
    
    * Add new proxy address 0x14F2982D601c9458F93bd70B218933A6f8165e7b
    
    * Apply linter fixes
    
    * fix: Refactor addresses
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Felipe Alvarado <felipe@safe.global>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -158,6 +158,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         ),  # v1.3.0+L2
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 63352908, "1.3.0"),  # v1.3.0
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 75118743, "1.5.0"),  # v1.5.0
+        (
+            "0xEdd160fEBBD92E350D4D398fb636302fccd67C7e",
+            75118747,
+            "1.5.0+L2",
+        ),  # v1.5.0+L2
     ],
     EthereumNetwork.POLYGON_ZKEVM: [
         (
@@ -4124,6 +4130,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             14306478,
         ),  # v1.3.0 default singleton address
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 75118687),  # v1.5.0
     ],
     EthereumNetwork.MUMBAI: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 13736914),  # v1.3.0
