fac412bb0dc11f2188905110d130bc67191c41a0
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-06 15:30:44 +0200

    Add addresses 1.5.0 for chain SONIC_MAINNET (#2686)
    * Add new master copy address 0xFf51A5898e281Db6DfC7855790607438dF2ca44b
    
    * Add new master copy address 0xEdd160fEBBD92E350D4D398fb636302fccd67C7e
    
    * Add new proxy address 0x14F2982D601c9458F93bd70B218933A6f8165e7b
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2569,6 +2569,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             5728153,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0xFf51A5898e281Db6DfC7855790607438dF2ca44b", 43713955, "1.5.0"),  # v1.5.0
+        (
+            "0xEdd160fEBBD92E350D4D398fb636302fccd67C7e",
+            77135106,
+            "1.5.0+L2",
+        ),  # v1.5.0+L2
     ],
     EthereumNetwork.WATERFALL_9_TEST_NETWORK: [
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 7329851, "1.4.1"),  # v1.4.1
@@ -5325,6 +5331,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 107724),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 79),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 5714149),  # v1.3.0
+        ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 43713929),  # v1.5.0
     ],
     EthereumNetwork.WATERFALL_9_TEST_NETWORK: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 7329623),  # v1.4.1
