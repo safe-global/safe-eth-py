ccbdbf9ebb5705a5904d499d433548be8d2b3c36
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-21 15:50:30 +0200

    Add addresses 1.3.0 for chain MIZUHIKI_MAINNET (#2707)
    * Add new chain 6498
    
    * Add new master copy address 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552
    
    * Add new master copy address 0x3E5c63644E683549055b9Be8653de26E0B4CD36E
    
    * Add new proxy address 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -4262,6 +4262,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.4.1+L2",
         ),  # v1.4.1+L2
     ],
+    EthereumNetwork.MIZUHIKI_MAINNET: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 20183, "1.3.0"),  # v1.3.0
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 20181, "1.3.0+L2"),  # v1.3.0+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -5900,6 +5904,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.NEO_X_MAINNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 10985),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 1604719),  # v1.4.1
+    ],
+    EthereumNetwork.MIZUHIKI_MAINNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 20167),  # v1.3.0
     ],
 }
 
