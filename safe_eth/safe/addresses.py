ff8cb855b25b4fcb1158c31546236782436566a9
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-08-27 11:27:44 +0200

    Add addresses 1.3.0 for chain ADI_NETWORK_AB_TESTNET (#2717)
    * Add new master copy address 0x69f4D1788e39c87893C980c06EdF4b7f686e2938
    
    * Add new master copy address 0xfb1bffC9d739B8D520DaF37dF666da4C687191EA
    
    * Add new proxy address 0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -4275,6 +4275,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.ADI_NETWORK_AB_TESTNET: [
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 606468, "1.4.1"),  # v1.4.1
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 606469, "1.4.1+L2"),  # v1.4.1+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 606701, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 606698, "1.3.0+L2"),  # v1.3.0+L2
     ],
 }
 
@@ -5923,6 +5925,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.ADI_NETWORK_AB_TESTNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 606458),  # v1.4.1
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 606687),  # v1.3.0
     ],
 }
 
