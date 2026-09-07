97518afed9fa344038481e57f00ae8f4078f2420
Safe Infra <104998789+Safe-infra@users.noreply.github.com>
2026-09-07 13:32:28 +0200

    Add addresses 1.3.0 for chain ZENITH_EVM_TESTNET (#2741)
    * Add new master copy address 0x69f4D1788e39c87893C980c06EdF4b7f686e2938
    
    * Add new master copy address 0xfb1bffC9d739B8D520DaF37dF666da4C687191EA
    
    * Add new proxy address 0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC
    
    * Apply linter fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -4313,6 +4313,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 225195, "1.4.1+L2"),  # v1.4.1+L2
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 225156, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 225158, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 225174, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 225176, "1.3.0+L2"),  # v1.3.0+L2
     ],
 }
 
@@ -5974,6 +5976,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x14F2982D601c9458F93bd70B218933A6f8165e7b", 225217),  # v1.5.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 225191),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 225154),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 225172),  # v1.3.0
     ],
 }
 
